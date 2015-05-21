f8aad37b21e08578fcac5fb04413935415a5745b
Yu Feng <yfeng1@berkeley.edu>
2015-05-20 22:07:08 -0700

    print the directory

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ git submodule init
 git submodule update
 for dir in MP-sort kdcount pfft-python pypm sharedmem; do
     (
+        echo $dir
         cd extern/$dir;
         python setup.py build_ext --inplace
     ) || exit
