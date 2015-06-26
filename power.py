75cda49eaad6edc3c83bb070b8c41d8188d9ddd9
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2015-06-26 15:51:29 -0700

    move lines around to make it look leaner.

--- power.py
+++ power.py
@@ -7,6 +7,7 @@ from argparse import ArgumentParser, RawTextHelpFormatter
 
 parser = ArgumentParser("Parallel Power Spectrum Calculator",
         formatter_class=RawTextHelpFormatter,
+        fromfile_prefix_chars="@",
         description=
      """Calculating matter power spectrum from RunPB input files. 
         Output is written to stdout, in Mpc/h units. 
@@ -23,6 +24,14 @@ parser = ArgumentParser("Parallel Power Spectrum Calculator",
 
 parser.add_argument("mode", choices=["2d", "1d"]) 
 
+parser.add_argument("BoxSize", type=float, 
+        help='BoxSize in Mpc/h')
+
+parser.add_argument("Nmesh", type=int, 
+        help='size of calculation mesh, recommend 2 * Ngrid')
+
+parser.add_argument("output", help='write power to this file') 
+
 parser.add_argument("--binshift", type=float, default=0.0,
         help='Shift the bin center by this fraction of the bin width. Default is 0.0. Marcel uses 0.5. this shall rarely be changed.' )
 parser.add_argument("--bunchsize", type=int, default=1024*1024*4,
@@ -31,7 +40,6 @@ parser.add_argument("--remove-cic", default='anisotropic', choices=["anisotropic
         help='deconvolve cic, anisotropic is the proper way, see http://www.personal.psu.edu/duj13/dissertation/djeong_diss.pdf')
 parser.add_argument("--Nmu", type=int, default=5,
         help='the number of mu bins to use' )
-
 
 class InputDesc(object):
     """ describing an input field. 
@@ -72,110 +80,7 @@ class InputDesc(object):
 
         return '\n'.join(rt)
 
-class HaloFilePainter(object):
-    def __init__(self, data): self.__dict__.update(data.__dict__)
-
-    @classmethod
-    def register(kls, inputdesc):
-        h = inputdesc.add_parser("HaloFile", 
-            usage="HaloFile:path:logMmin:logMmax:m0[:-rsd=[x|y|z]]",
-            )
-        h.add_argument("path", help="path to file")
-        h.add_argument("logMmin", type=float, help="log10 min mass")
-        h.add_argument("logMmax", type=float, help="log10 max mass")
-        h.add_argument("m0", type=float, help="mass mass of a particle")
-        h.add_argument("-rsd", 
-            choices="xyz", help="direction to do Redshift distortion")
-
-        h.set_defaults(klass=kls)
-
-    def paint(self, ns, pm):
-        if pm.comm.rank == 0:
-            hf = files.HaloFile(self.path)
-            nhalo = hf.nhalo
-            halopos = numpy.float32(hf.read_pos())
-            halovel = numpy.float32(hf.read_vel())
-            halomass = numpy.float32(hf.read_mass() * self.m0)
-            logmass = numpy.log10(halomass)
-            mask = logmass > self.logMmin
-            mask &= logmass < self.logMmax
-            halopos = halopos[mask]
-            halovel = halovel[mask]
-            logging.info("total number of halos in mass range is %d" % mask.sum())
-        else:
-            halopos = numpy.empty((0, 3), dtype='f4')
-            halovel = numpy.empty((0, 3), dtype='f4')
-            halomass = numpy.empty(0, dtype='f4')
-
-        Ntot = len(halopos)
-        Ntot = pm.comm.bcast(Ntot)
-
-        if self.rsd is not None:
-            dir = 'xyz'.index(self.rsd)
-            halopos[:, dir] += halovel[:, dir]
-        halopos *= ns.BoxSize
-
-        layout = pm.decompose(halopos)
-        tpos = layout.exchange(halopos)
-        pm.paint(tpos)
-
-        npaint = pm.comm.allreduce(len(tpos), op=MPI.SUM) 
-        return Ntot
-
-class TPMSnapshotPainter(object):
-    def __init__(self, data):
-        self.__dict__.update(data.__dict__)
-
-    @classmethod
-    def register(kls, inputdesc):
-        h = inputdesc.add_parser("TPMSnapshot", 
-            usage="TPMSnapshot:path[:-rsd=[x|y|z]]")
-        h.add_argument("path", help="path to file")
-        h.add_argument("-rsd", 
-            choices="xyz", default=None, help="direction to do Redshift distortion")
-        h.set_defaults(klass=kls)
-
-    def paint(self, ns, pm):
-        pm.real[:] = 0
-        Ntot = 0
-        for round, P in enumerate(
-                files.read(pm.comm, 
-                    self.path, 
-                    files.TPMSnapshotFile, 
-                    columns=['Position', 'Velocity'], 
-                    bunchsize=ns.bunchsize)):
-
-            nread = pm.comm.allreduce(len(P['Position']), op=MPI.SUM) 
-
-            if self.rsd is not None:
-                dir = "xyz".index(self.rsd)
-                P['Position'][:, dir] += P['Velocity'][:, dir]
-
-            P['Position'] *= ns.BoxSize
-            layout = pm.decompose(P['Position'])
-            tpos = layout.exchange(P['Position'])
-            #print tpos.shape
-            pm.paint(tpos)
-            npaint = pm.comm.allreduce(len(tpos), op=MPI.SUM) 
-            if pm.comm.rank == 0:
-                logging.info('round %d, npaint %d, nread %d' % (round, npaint, nread))
-            Ntot = Ntot + nread
-        return Ntot
-
-TPMSnapshotPainter.register(InputDesc) 
-HaloFilePainter.register(InputDesc) 
-
-parser.add_argument("BoxSize", type=float, 
-        help='BoxSize in Mpc/h')
-parser.add_argument("Nmesh", type=int, 
-        help='size of calculation mesh, recommend 2 * Ngrid')
-parser.add_argument("output", help='write power to this file') 
-
 parser.add_argument("input", nargs='+', type=InputDesc, help=InputDesc.format_help())
-
-ns = parser.parse_args()
-
-# done with the parser. now do the real calculation
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -191,6 +96,8 @@ from pypm.transfer import TransferFunction
 from mpi4py import MPI
 
 def main():
+
+    ns = parser.parse_args()
 
     if MPI.COMM_WORLD.rank == 0:
         print 'importing done'
@@ -258,5 +165,99 @@ def do1d(pm, complex, ns):
             myout = stdout
         numpy.savetxt(myout, zip(k, p), '%0.7g')
         myout.flush()
+
+class HaloFilePainter(object):
+    def __init__(self, data): 
+        self.__dict__.update(data.__dict__)
+
+    @classmethod
+    def register(kls, inputdesc):
+        h = inputdesc.add_parser("HaloFile", 
+            usage="HaloFile:path:logMmin:logMmax:m0[:-rsd=[x|y|z]]",
+            )
+        h.add_argument("path", help="path to file")
+        h.add_argument("logMmin", type=float, help="log10 min mass")
+        h.add_argument("logMmax", type=float, help="log10 max mass")
+        h.add_argument("m0", type=float, help="mass mass of a particle")
+        h.add_argument("-rsd", 
+            choices="xyz", help="direction to do Redshift distortion")
+
+        h.set_defaults(klass=kls)
+
+    def paint(self, ns, pm):
+        if pm.comm.rank == 0:
+            hf = files.HaloFile(self.path)
+            nhalo = hf.nhalo
+            halopos = numpy.float32(hf.read_pos())
+            halovel = numpy.float32(hf.read_vel())
+            halomass = numpy.float32(hf.read_mass() * self.m0)
+            logmass = numpy.log10(halomass)
+            mask = logmass > self.logMmin
+            mask &= logmass < self.logMmax
+            halopos = halopos[mask]
+            halovel = halovel[mask]
+            logging.info("total number of halos in mass range is %d" % mask.sum())
+        else:
+            halopos = numpy.empty((0, 3), dtype='f4')
+            halovel = numpy.empty((0, 3), dtype='f4')
+            halomass = numpy.empty(0, dtype='f4')
+
+        Ntot = len(halopos)
+        Ntot = pm.comm.bcast(Ntot)
+
+        if self.rsd is not None:
+            dir = 'xyz'.index(self.rsd)
+            halopos[:, dir] += halovel[:, dir]
+        halopos *= ns.BoxSize
+
+        layout = pm.decompose(halopos)
+        tpos = layout.exchange(halopos)
+        pm.paint(tpos)
+
+        npaint = pm.comm.allreduce(len(tpos), op=MPI.SUM) 
+        return Ntot
+HaloFilePainter.register(InputDesc) 
+
+class TPMSnapshotPainter(object):
+    def __init__(self, data):
+        self.__dict__.update(data.__dict__)
+
+    @classmethod
+    def register(kls, inputdesc):
+        h = inputdesc.add_parser("TPMSnapshot", 
+            usage="TPMSnapshot:path[:-rsd=[x|y|z]]")
+        h.add_argument("path", help="path to file")
+        h.add_argument("-rsd", 
+            choices="xyz", default=None, help="direction to do Redshift distortion")
+        h.set_defaults(klass=kls)
+
+    def paint(self, ns, pm):
+        pm.real[:] = 0
+        Ntot = 0
+        for round, P in enumerate(
+                files.read(pm.comm, 
+                    self.path, 
+                    files.TPMSnapshotFile, 
+                    columns=['Position', 'Velocity'], 
+                    bunchsize=ns.bunchsize)):
+
+            nread = pm.comm.allreduce(len(P['Position']), op=MPI.SUM) 
+
+            if self.rsd is not None:
+                dir = "xyz".index(self.rsd)
+                P['Position'][:, dir] += P['Velocity'][:, dir]
+
+            P['Position'] *= ns.BoxSize
+            layout = pm.decompose(P['Position'])
+            tpos = layout.exchange(P['Position'])
+            #print tpos.shape
+            pm.paint(tpos)
+            npaint = pm.comm.allreduce(len(tpos), op=MPI.SUM) 
+            if pm.comm.rank == 0:
+                logging.info('round %d, npaint %d, nread %d' % (round, npaint, nread))
+            Ntot = Ntot + nread
+        return Ntot
+
+TPMSnapshotPainter.register(InputDesc) 
 
 main()
