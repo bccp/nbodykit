aa12685e408e0f08353b354f7638af07fb5d2a69
Nick Hand <nhand@berkeley.edu>
2015-07-05 15:35:49 -0700

    now properly parsing arguments with nargs='+' into list

--- nbodykit/plugins/PandasPlainTextPainter.py
+++ nbodykit/plugins/PandasPlainTextPainter.py
@@ -4,6 +4,9 @@ import numpy
 import logging
 from nbodykit import files
 
+def list_str(value):
+    return value.split()
+
 #------------------------------------------------------------------------------          
 class PandasPlainTextPainter(InputPainter):
     field_type = "PandasPlainText"
@@ -12,24 +15,24 @@ class PandasPlainTextPainter(InputPainter):
     def register(kls):
         
         args = kls.field_type+":path:names"
-        options = "[:-usecols='x' 'y' 'z'][:-poscols='x' 'y' 'z']\n[:-velcols='vx' 'vy' 'vz']" + \
+        options = "[:-usecols= x y z][:-poscols= x y z]\n[:-velcols= vx vy vz]" + \
                   "[:-rsd=[x|y|z]][:-posf=0.001][:-velf=0.001][:-select=flags]"
         h = kls.add_parser(kls.field_type, usage=args+options)
         
         h.add_argument("path", help="path to file")
-        h.add_argument("names", nargs='+', type=str, 
+        h.add_argument("names", type=list_str, 
             help="names of columns in file")
-        h.add_argument("-usecols", nargs='+', type=str, 
+        h.add_argument("-usecols", type=list_str, 
             help="only read these columns from file")
-        h.add_argument("-poscols", nargs=3, default=['x','y','z'], 
+        h.add_argument("-poscols", type=list_str, default=['x','y','z'], 
             help="names of the position columns")
-        h.add_argument("-velcols", nargs=3, default=None,
+        h.add_argument("-velcols", type=list_str, default=None,
             help="names of the velocity columns")
         h.add_argument("-rsd", choices="xyz", 
             help="direction to do redshift distortion")
-        h.add_argument("-posf", default=0.001, type=float, 
+        h.add_argument("-posf", default=1., type=float, 
             help="factor to scale the positions")
-        h.add_argument("-velf", default=0.001, type=float, 
+        h.add_argument("-velf", default=1., type=float, 
             help="factor to scale the velocities")
         h.add_argument("-select", default=None, type=files.FileSelection, 
             help='row selection based on flags specified as string')
