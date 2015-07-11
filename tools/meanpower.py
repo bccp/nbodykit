f07f5d0cce7d66453a1b6aebc6169e402646e46d
Nick Hand <nhand@berkeley.edu>
2015-07-10 17:38:22 -0700

    can now loop over several batch strings

--- tools/meanpower.py
+++ tools/meanpower.py
@@ -23,57 +23,84 @@ h = 'the pattern to match files power files on'
 parser.add_argument('pattern', type=str, help=h)
 h = 'the name of the output file'
 parser.add_argument('output', default=None, type=str, help=h)
+h = 'loop over these arguments, doing a string replace on the ' + \
+    'input/output files, using %s format'
+parser.add_arguments('--batch', args='+', type=str, help=h)
 
 args = parser.parse_args()
 
 def main():
 
-    # read the files
-    results = glob(args.pattern)
-    if not len(results):
-        raise RuntimeError("whoops, no files match input pattern")
+    nbatch = 1
+    if args.batch is not None:
+        nbatch = len(args.batch)
     
-    # loop over each file
-    data = []
-    for f in results:
-        if args.mode == '2d':
-            try:
-                d, meta = files.ReadPower2DPlainText(f)
-            except Exception as e:
-                raise RuntimeError("error reading `%s` as a power 2D plain text file: %s" %(f, str(e)))
-            data.append(pkmuresult.PkmuResult.from_dict(d, **meta))
-        else:
-            data.append(numpy.loadtxt(f))
+    # do each pattern provided
+    for n in range(nbatch):
         
-    # average and output
-    if args.mode == '2d':
-        avg = pkmuresult.PkmuResult.from_list(data, sum_only=['modes'], weights='modes')
-        output = plugins.Power2DStorage(args.output)
-    
-        data = {k:avg[k].data for k in avg.columns}
-        data['edges'] = [avg.kedges, avg.muedges]
-        meta = {k:getattr(avg,k) for k in avg._metadata}
-        output.write(data, **meta)
-    else:        
-        avg = []
-        for i, name in zip(range(3), ['k', 'power', 'modes']):
+        # try to replace with the batch string
+        pattern = args.pattern
+        output_file = args.output
+        if args.batch is not None:
             
-            if name == 'modes':
-                avg.append(numpy.sum([d[:,i] for d in data], axis=0))
-            else:
-                # mask any element that is NaN for all spectra we are avging
-                mask = numpy.ones(data[0].shape[0], dtype=bool)
-                for d in data:
-                    mask &= numpy.isnan(d[:,i])
+            # do the pattern
+            fmt_count = pattern.find('%s')
+            if fmt_count > 0:
+                args = (args.nbatch[n])*fmt_count
+                pattern = pattern %args
                 
-                colavg = numpy.empty(mask.shape)
-                weights = [d[:,2][~mask] for d in data] # weight by modes
-                colavg[~mask] = numpy.average([d[:,i][~mask] for d in data], axis=0, weights=weights)
-                colavg[mask] = numpy.nan
-                avg.append(colavg)
+            # and the output file
+            fmt_count = output_file.find('%s')
+            if fmt_count > 0:
+                args = (args.nbatch[n])*fmt_count
+                output_file = output_file %args
+        
+        # read the files
+        results = glob(pattern)
+        if not len(results):
+            raise RuntimeError("whoops, no files match input pattern")
+    
+        # loop over each file
+        data = []
+        for f in results:
+            if args.mode == '2d':
+                try:
+                    d, meta = files.ReadPower2DPlainText(f)
+                except Exception as e:
+                    raise RuntimeError("error reading `%s` as a power 2D plain text file: %s" %(f, str(e)))
+                data.append(pkmuresult.PkmuResult.from_dict(d, **meta))
+            else:
+                data.append(numpy.loadtxt(f))
+        
+        # average and output
+        if args.mode == '2d':
+            avg = pkmuresult.PkmuResult.from_list(data, sum_only=['modes'], weights='modes')
+            output = plugins.Power2DStorage(output_file)
+    
+            data = {k:avg[k].data for k in avg.columns}
+            data['edges'] = [avg.kedges, avg.muedges]
+            meta = {k:getattr(avg,k) for k in avg._metadata}
+            output.write(data, **meta)
+        else:        
+            avg = []
+            for i, name in zip(range(3), ['k', 'power', 'modes']):
+            
+                if name == 'modes':
+                    avg.append(numpy.sum([d[:,i] for d in data], axis=0))
+                else:
+                    # mask any element that is NaN for all spectra we are avging
+                    mask = numpy.ones(data[0].shape[0], dtype=bool)
+                    for d in data:
+                        mask &= numpy.isnan(d[:,i])
+                
+                    colavg = numpy.empty(mask.shape)
+                    weights = [d[:,2][~mask] for d in data] # weight by modes
+                    colavg[~mask] = numpy.average([d[:,i][~mask] for d in data], axis=0, weights=weights)
+                    colavg[mask] = numpy.nan
+                    avg.append(colavg)
 
-        output = plugins.Power1DStorage(args.output) 
-        output.write(avg)
+            output = plugins.Power1DStorage(output_file) 
+            output.write(avg)
     
 if __name__ == '__main__':
     main()
