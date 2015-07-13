c44b2079fed9b0ff2fb6c864506ba0baa12074de
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2015-07-13 12:07:30 -0700

    allow using centrals and satellites together.
    e.g. python power.py -X contrib/MBIIDMO.py 1d 100. 256 mbiidmo.txt MBIIDMO:mb2dmo/datato_nickhand/z0.6/:mb2:Both:-1:1000:-rsd=z

--- contrib/MBIIDMO.py
+++ contrib/MBIIDMO.py
@@ -8,8 +8,10 @@ class PainterPlugin(InputPainter):
     @classmethod
     def register(kls):
         h = kls.add_parser(kls.field_type, 
-            usage=kls.field_type+":path:logMmin:logMmax:[:-rsd=[x|y|z]][:-posf=0.001][:-velf=0.001]")
+            usage=kls.field_type+":path:type:logMmin:logMmax:[:-rsd=[x|y|z]][:-posf=0.001][:-velf=0.001]")
         h.add_argument("path", help="path to file")
+        h.add_argument("simulation", help="name of simulation", choices=["dmo", "mb2"])
+        h.add_argument("type", help="type of objects", choices=["Centrals", "Satellites", "Both"])
         h.add_argument("logMmin", help="log 10 Mmin", type=float)
         h.add_argument("logMmax", help="log 10 Mmax", type=float)
         h.add_argument("-rsd", 
@@ -20,14 +22,20 @@ class PainterPlugin(InputPainter):
                 help="factor to scale the velocities", type=float)
         h.set_defaults(klass=kls)
 
+    def read_block(self, block, dtype):
+        if self.type == "Both":
+            types = "Centrals", "Satellites"
+        else:
+            types = [self.type]
+        return numpy.concatenate([
+            numpy.fromfile(self.path + '/' + type + '/' + self.simulation + '_' + block, dtype=dtype)
+            for type in types], axis=0)
+            
     def paint(self, ns, pm):
         if pm.comm.rank == 0:
-            pos = numpy.fromfile(
-                self.path + '_pos', dtype=('f4', 3))
-            vel = numpy.fromfile(
-                self.path + '_vel', dtype=('f4', 3))
-            mass = numpy.fromfile(
-                self.path + '_mass', dtype='f8')
+            pos = self.read_block('pos', ('f4', 3))
+            vel = self.read_block('vel', ('f4', 3))
+            mass = self.read_block('mass', ('f8'))
             logmass = numpy.log10(mass)
             sel = (self.logMmin < logmass) & (self.logMmax > logmass)
 
