73824cbceac22d1d91e5eb82b10101a28498169f
Nick Hand <nhand@berkeley.edu>
2015-08-17 18:58:10 -0700

    adding painter for BOSS QPM mocks

--- contrib/QPMMock.py
+++ contrib/QPMMock.py
@@ -1,11 +1,10 @@
 from nbodykit.plugins import InputPainter
-
 import numpy
 import logging
          
-class BOSSChallengeMockPainter(InputPainter):
+class QPMMockPainter(InputPainter):
     """
-    Class to read data from the DR12 BOSS periodic box challenge 
+    Class to read data from the DR12 BOSS QPM periodic box 
     mocks, which are stored as a plain text ASCII file, and 
     paint the field onto a density grid. The data is read
     from file using `pandas.read_csv` and is stored internally in 
@@ -14,30 +13,36 @@ class BOSSChallengeMockPainter(InputPainter):
     Notes
     -----
     * `pandas` must be installed to use
-    * first three columns are `x`, `y`, `z`
-    * data is assumed to be in redshift-space, with `z` giving
-    the LOS axis
+    * columns are `x`, `y`, `z`, `vx`, `vy`, `vz`
     
     Parameters
     ----------
-    path    : str
+    path   : str
         the path of the file to read the data from 
     scaled : bool, optional
         rescale the parallel and perp coordinates by the AP factor
+    rsd    : [x|y|z], optional
+        direction to do the redshift space distortion
+    velf   : float, optional
+        multiply the velocity data by this factor
     """
-    field_type = 'BOSSChallengeMock'
-    qpar = 1.0
-    qperp = 1.0
+    field_type = 'QPMMock'
+    qpar = 1.020096
+    qperp = 1.027742
     
     @classmethod
     def register(kls):
         
-        usage = kls.field_type+":path[:-scaled]"
+        usage = kls.field_type+":path[:-scaled][:-rsd][:-velf]"
         h = kls.add_parser(kls.field_type, usage=usage)
         
         h.add_argument("path", help="path to file")
         h.add_argument("-scaled", action='store_true', 
             help='rescale the parallel and perp coordinates by the AP factor')
+        h.add_argument("-rsd", choices="xyz",
+            help="direction to do redshift distortion")
+        h.add_argument("-velf", default=1., type=float, 
+            help="factor to scale the velocities")
         h.set_defaults(klass=kls)
     
     def paint(self, ns, pm):
@@ -45,7 +50,7 @@ class BOSSChallengeMockPainter(InputPainter):
             try:
                 import pandas as pd
             except:
-                raise ImportError("pandas must be installed to use BOSSChallengeMockPainter")
+                raise ImportError("pandas must be installed to use QPMMockPainter")
                 
             # read in the plain text file using pandas
             kwargs = {}
@@ -62,21 +67,35 @@ class BOSSChallengeMockPainter(InputPainter):
             
             # get position 
             pos = data[['x', 'y', 'z']].values.astype('f4')
+            vel = data[['x', 'y', 'z']].values.astype('f4')
+            vel *= self.velf
         else:
             pos = numpy.empty(0, dtype=('f4', 3))
+            vel = numpy.empty(0, dtype=('f4', 3))
 
         Ntot = len(pos)
         Ntot = pm.comm.bcast(Ntot)
 
-        # assumed the position values are now in same
-        # units as ns.BoxSize
+        # go to redshift-space
+        if self.rsd is not None:
+            dir = 'xyz'.index(self.rsd)
+            pos[:, dir] += vel[:, dir]
+            pos[:, dir] %= ns.BoxSize # enforce periodic boundary conditions
+        
+        # rescale by AP factor
         if self.scaled:
             if pm.comm.rank == 0:
                 logging.info("multiplying by qperp = %.5f" %self.qperp)
                 logging.info("multiplying by qpar = %.5f" %self.qpar)
-            pos[:,0] *= self.qperp
-            pos[:,1] *= self.qperp
-            pos[:,2] *= self.qpar
+            if self.rsd is None:
+                pos *= self.qperp
+            else:
+                other = [x for x in [0,1,2] if x != dir]
+                for i in [1,2,3]:
+                    if i == dir:
+                        pos[:,i] *= self.qpar
+                    else:
+                        pos[:,i] *= self.qperp
 
         layout = pm.decompose(pos)
         tpos = layout.exchange(pos)
@@ -84,41 +103,6 @@ class BOSSChallengeMockPainter(InputPainter):
 
         npaint = pm.comm.allreduce(len(tpos)) 
         return Ntot
-        
-class BOSSChallengeBoxAPainter(BOSSChallengeMockPainter):
-    field_type = 'BOSSChallengeBoxA'
-    qperp = 0.998753592
-    qpar = 0.9975277944
-    
-class BOSSChallengeBoxBPainter(BOSSChallengeMockPainter):
-    field_type = 'BOSSChallengeBoxB'
-    qperp = 0.9875682111
-    qpar = 0.9751013789
-    
-class BOSSChallengeBoxCPainter(BOSSChallengeMockPainter):
-    field_type = 'BOSSChallengeBoxC'
-    qperp = 0.9875682111
-    qpar = 0.9751013789
-    
-class BOSSChallengeBoxDPainter(BOSSChallengeMockPainter):
-    field_type = 'BOSSChallengeBoxD'
-    qperp = 0.9916978595
-    qpar = 0.9834483344
-    
-class BOSSChallengeBoxEPainter(BOSSChallengeMockPainter):
-    field_type = 'BOSSChallengeBoxE'
-    qperp = 0.9916978595
-    qpar = 0.9834483344
-    
-class BOSSChallengeBoxFPainter(BOSSChallengeMockPainter):
-    field_type = 'BOSSChallengeBoxF'
-    qperp = 0.998753592
-    qpar = 0.9975277944
-    
-class BOSSChallengeBoxGPainter(BOSSChallengeMockPainter):
-    field_type = 'BOSSChallengeBoxG'
-    qperp = 0.998753592
-    qpar = 0.9975277944
 
 
     
