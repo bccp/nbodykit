f154c30335eff07f38f2cf380acf700401dcf786
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2015-08-28 12:15:35 -0700

    Move Painters to DataSource directory.

--- nbodykit/plugins/__init__.py
+++ nbodykit/plugins/__init__.py
@@ -184,8 +184,7 @@ def load(filename, namespace=None):
         raise RuntimeError("Failed to load plugin '%s': %s" % (filename, str(e)))
     references[filename] = namespace
 
-builtins = ['TPMSnapshotPainter', 'HaloFilePainter', 'PandasPainter',
-            'PlainTextPainter', 'Power1DStorage', 'Power2DStorage']
+builtins = ['DataSource', 'Power1DStorage.py', 'Power2DStorage.py']
 for plugin in builtins:
-    load(os.path.join(os.path.dirname(__file__), plugin + '.py'))
+    load(os.path.join(os.path.dirname(__file__), plugin))
  
