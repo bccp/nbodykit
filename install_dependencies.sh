9b089573f974538fb9d6bd129a4c2b6bc1596c32
Yu Feng <yfeng1@berkeley.edu>
2015-11-15 17:44:53 -0800

    More tweaks at Edison.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,25 +1,37 @@
-SOURCE=`dirname $0`/depends
-LOCAL=`dirname $0`/install
-LOCAL=`realpath $LOCAL`
+SOURCE="`dirname $0`/depends"
+LOCAL="`dirname $0`/install"
+mkdir -p "$LOCAL"
+
+LOCAL="`cd "$LOCAL";pwd`"
+
+if [ "x$MPICC" == "x" ]; then
+    MPICC=mpicc
+fi
+
+if ! $MPICC --version; then
+    echo The compiler MPICC="\"$MPICC\"" does not work. 
+    exit 1
+fi
 
 function fail {
     echo Installation of $1 failed
     # list common pitfalls
     # echo Common pitfalls
+    exit 1
 }
 
-mkdir -p $SOURCE/
+mkdir -p "$SOURCE"
 
 function install {
     echo Installing $1 to $LOCAL
     (
-    cd $SOURCE
+    cd "$SOURCE"
     if ! [ -d $2 ]; then
-        git clone $1 $2 
+        git clone "$1" "$2"  || exit 1
     fi
-    cd $2 && git checkout -f master && git pull
-    python setup.py install --prefix=$LOCAL
-    ) || fail $1
+    cd "$2" && git checkout -f master && git pull || exit 1
+    python setup.py install --prefix="$LOCAL" || exit 1
+    ) || fail "$1"
 }
 
 install http://github.com/rainwoodman/sharedmem sharedmem
