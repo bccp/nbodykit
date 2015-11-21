0ad5bd996b4a153005e14f9f6e7ac490e517150e
Nick Hand <nhand@berkeley.edu>
2015-11-20 22:49:53 -0800

    load the new measurement storage classes

--- nbodykit/__init__.py
+++ nbodykit/__init__.py
@@ -2,7 +2,7 @@ from nbodykit.plugins import load
 import os.path
 path = os.path.abspath(os.path.dirname(__file__))
 
-builtins = ['datasource/', 'plugins/Power1DStorage.py', 'plugins/Power2DStorage.py']
+builtins = ['datasource/', 'plugins/Measurement1DStorage.py', 'plugins/Measurement2DStorage.py']
 
 for plugin in builtins:
     load(os.path.join(path, plugin))
