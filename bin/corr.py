489b32f67fd493bbae1a20966bf65ab5b6c729cf
Nick Hand <nhand@berkeley.edu>
2015-11-23 11:50:42 -0800

    moving brutal corr calc to function in measurestats

--- bin/corr.py
+++ bin/corr.py
@@ -1,7 +1,5 @@
 import logging
-import warnings
 from mpi4py import MPI
-import numpy
 
 rank = MPI.COMM_WORLD.rank
 name = MPI.Get_processor_name()
@@ -9,25 +7,14 @@ logging.basicConfig(level=logging.DEBUG,
                     format='rank %d on %s: '%(rank,name) + \
                             '%(asctime)s %(name)-15s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M')
-logger = logging.getLogger('power.py')
+logger = logging.getLogger('corr.py')
               
-import nbodykit
-from nbodykit import plugins
+from nbodykit import plugins, measurestats
 from nbodykit.utils.pluginargparse import PluginArgumentParser
-from kdcount import correlate
-from pmesh.domain import GridND
 
 def initialize_parser(**kwargs):
-    parser = PluginArgumentParser("Brutal Correlation function Calculator",
-            loader=plugins.load,
-            description=
-         """
-         """,
-            epilog=
-         """
-         """,
-            **kwargs
-         )
+    parser = PluginArgumentParser("Brutal Correlation unction Calculator",
+                                    loader=plugins.load, **kwargs)
 
     # add the positional arguments
     parser.add_argument("output", help='write correlation function to this file. set as `-` for stdout') 
@@ -41,81 +28,35 @@ def initialize_parser(**kwargs):
 
     # add the optional arguments
     parser.add_argument("--subsample", type=int, default=1,
-        help='Use 1 out of every N points')
+                        help='Use 1 out of every N points')
     return parser 
+
+
+def compute_brutal_corr(ns, comm=None):
+    """
+    
+    """
+    if comm is None: comm = MPI.COMM_WORLD
+    
+    # call the function
+    kw = {'comm':comm, 'subsample':ns.subsample}
+    pc, xi, RR = measurestats.compute_brutal_3d_corr(ns.inputs, ns.rmax, ns.Nbins, **kw)
+    
+    
+    if comm.rank == 0:
+        storage = plugins.MeasurementStorage.new('1d', ns.output)
+        storage.write(pc.edges, ['r', 'corr', 'RR'], (pc.centers, xi, RR))
+        logger.info('brutal corr done')
 
 def main():
     """
-    The main function to initialize the parser and do the work
+    The main function
     """
     # parse
-
     ns = initialize_parser().parse_args()
-    comm = MPI.COMM_WORLD
-    for Nx in range(int(comm.size **0.3333) + 1, 0, -1):
-        if comm.size % Nx == 0: break
-    else:
-        Nx = 1
-    for Ny in range(int(comm.size **0.5) + 1, 0, -1):
-        if (comm.size // Nx) % Ny == 0: break
-    else:
-        Ny = 1
-    Nz = comm.size // Nx // Ny
-
-    Nproc = [Nx, Ny, Nz]
-    if comm.rank == 0:
-        logger.info('Nproc = %s' % str( Nproc))
-        logger.info('rmax = %g' % ns.rmax)
-    domain = GridND([
-            numpy.linspace(0, ns.inputs[0].BoxSize[i], Nproc[i] + 1, endpoint=True)
-            for i in range(3)])
-
-    [[pos1]] = ns.inputs[0].read(['Position'], comm, full=True)
-    pos1 = pos1[comm.rank * ns.subsample // comm.size ::ns.subsample]
-    N1 = comm.allreduce(len(pos1))
-    if len(ns.inputs) > 1:
-        [[pos2]] = ns.inputs[1].read(['Position'], comm, full=True)
-        pos2 = pos2[comm.rank * ns.subsample // comm.size ::ns.subsample]
-    else:
-        pos2 = pos1
-    N2 = comm.allreduce(len(pos2))
-    if comm.rank == 0:
-        logger.info('Read Positions %d points', len(pos1))
-    layout = domain.decompose(pos1, smoothing=0)
-    pos1 = layout.exchange(pos1)
-    if comm.rank == 0:
-        logger.info('exchange pos1')
-    if ns.rmax > ns.inputs[0].BoxSize[0] * 0.25:
-        pos2 = numpy.concatenate(comm.allgather(pos2), axis=0)
-    else:
-        layout = domain.decompose(pos2, smoothing=ns.rmax)
-        pos2 = layout.exchange(pos2)
-    if comm.rank == 0:
-        logger.info('exchange pos2')
-
-    tree1 = correlate.points(pos1, boxsize=ns.inputs[0].BoxSize)
-    tree2 = correlate.points(pos2, boxsize=ns.inputs[0].BoxSize)
-
-    if comm.rank == 0:
-        logger.info('Rank 0 correlating %d x %d' % (len(tree1), len(tree2)))
-
-    if comm.rank == 0:
-        logger.info('All correlating %d x %d' % (N1, N2))
-
-    bins = correlate.RBinning(ns.rmax, ns.Nbins)
-
-    pc = correlate.paircount(tree2, tree1, bins, np=0)
-    pc.sum1[:] = comm.allreduce(pc.sum1)
-
-    RR = 1.0 * N1 * N2
-    RR *= 4. / 3. * numpy.pi * numpy.diff(pc.edges**3/ ns.inputs[0].BoxSize.prod())
-
-    xi = 1.0 * pc.sum1 / RR - 1
     
-    if comm.rank == 0:
-        storage = plugins.PowerSpectrumStorage.new('1d', ns.output)
-        storage.write((pc.centers, xi, RR))
-        logger.info('done')
-
+    # do the work
+    compute_brutal_corr(ns)
+    
 if __name__ == '__main__':
     main()
