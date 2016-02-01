0046a1b11884a1a2f738aca423480a49e74b4617
Nick Hand <nhand@berkeley.edu>
2016-01-31 22:37:43 -0800

    first pass at implementing `Algorithms` with correlation example
    the idea is to take a look at this interface and see if there are any
    major issues. the only working example so far (replacing corr.py) seems
    relatively easy to use and pretty clean I think
    
    * `nbkit.py` is the new main executable -- each algorithm has its own subcommand
    * right now, the only working example is `PairCountCorrelation` which
    effectively replaces `corr.py` -- see examples/corr for working examples
    
    some other key points / features:
    * Algorithm plugin has a normal __init__ and the `create` function converts
    a command-line Namespace to the format needed by the __init__
    * added `algorithms` Namespace to `extensionpoints` module that will store the
    Algorithm plugins to make calling algorithms in interactive mode easier
    * default arguments for the `parser` attribute of each Algorithm class are
    set automatically using the keyword argument values in the __init__
    * not exactly sold on how the `output` string is implemented now in `nbkit.py`
    but we can adapt as we have more algorithms implemented I think

---
+++ nbodykit/plugins/algorithms/PairCountCorrelation.py
@@ -0,0 +1,143 @@
+from nbodykit.extensionpoints import Algorithm
+from nbodykit.plugins import ListPluginsAction
+import numpy
+import os
+
+def binning_type(s):
+    """
+    Type conversion for use on the command-line that converts 
+    a string to an array of bin edges
+    """
+    if os.path.isfile(s):
+        return numpy.loadtxt(s)
+    else:
+        supported = ["`linspace: min max Nbins`", "`logspace: logmin logmax Nbins`"]
+        try:
+            f, params = s.split(':')
+            params = list(map(float, params.split()))
+            params[-1] = int(params[-1]) + 1
+
+            if not hasattr(numpy, f): raise Exception
+            if len(params) != 3: raise Exception
+
+            return getattr(numpy, f)(*params)
+        except:
+            raise TypeError("supported binning format: [ %s ]" %", ".join(supported))
+
+class PairCountCorrelationAlgorithm(Algorithm):
+    """
+    Algorithm to compute the 1d or 2d correlation function and multipoles
+    via direct pair counting
+    """
+    plugin_name = "PairCountCorrelation"
+
+    def __init__(self, mode, rbins, inputs, subsample=1, los='z', Nmu=10, poles=[]):
+        """
+        See `print_help()` for documentation on parameters
+        """
+        self.mode      = mode
+        self.rbins     = rbins
+        self.inputs    = inputs
+        self.subsample = subsample
+        self.los       = los
+        self.Nmu       = Nmu
+        self.poles     = poles
+
+    @classmethod
+    def register(kls):
+        """
+        Setup the argument parser needed for initialization via the command-line
+        """
+        from nbodykit.extensionpoints import DataSource
+        
+        p = kls.parser
+        p.description = "correlation function calculator via pair counting"
+    
+        # the positional arguments
+        p.add_argument("mode", choices=["1d", "2d"],
+                        help='measure the correlation function in `1d` or `2d`') 
+        p.add_argument("rbins", type=binning_type, 
+                        help='the string specifying the binning to use') 
+        p.add_argument("inputs", nargs="+", type=DataSource.create, 
+                        help='1 or 2 input `DataSource` objects to correlate; run --list-datasource for specifics')
+
+        # add the optional arguments
+        p.add_argument("--subsample", type=int,
+                        help='use 1 out of every N points')
+        p.add_argument("--los", choices="xyz",
+                        help="the line-of-sight: the angle `mu` is defined with respect to")
+        p.add_argument("--Nmu", type=int,
+                        help='if `mode == 2d`, the number of mu bins covering mu=[-1,1]')
+        p.add_argument('--poles', type=lambda s: [int(i) for i in s.split()], metavar="0 2 4",
+                        help='compute the multipoles for these `ell` values from xi(r,mu)')
+        p.add_argument("--list-datasource", action=ListPluginsAction(DataSource),
+                        help='list the help for each available `DataSource`')
+
+    def run(self, comm=None):
+        """
+        Run the pair-count correlation function and return the result
+        
+        Returns
+        -------
+        edges : list or array_like
+            the array of 1d bin edges or a list of the bin edges in each dimension
+        result : dict
+            dictionary holding the data results (with associated names as keys) --
+            this results `corr`, `RR`, `N` + the mean bin values
+        """
+        from mpi4py import MPI
+        from nbodykit import measurestats
+        
+        if comm is None: comm = MPI.COMM_WORLD
+
+        # check multipoles parameters
+        if len(self.poles) and self.mode == '2d':
+            raise ValueError("you specified multipole numbers but `mode` is `2d` -- perhaps you meant `1d`")
+
+        # set Nmu to 1 if doing 1d
+        if self.mode == "1d": self.Nmu = 0
+
+        # do the work
+        kw = {'comm':comm, 'subsample':self.subsample, 'Nmu':self.Nmu, 'los':self.los, 'poles':self.poles}
+        pc, xi, RR = measurestats.compute_brutal_corr(self.inputs, self.rbins, **kw)
+
+        # format the results
+        if self.mode == '1d':
+            if len(self.poles):
+                cols = ['r'] + ['corr_%d' %l for l in self.poles] + ['RR', 'N']
+                result = [pc.mean_centers] + [xi[:,i] for i in range(len(self.poles))] + [RR, pc.pair_counts]
+            else:
+                cols = ['r', 'corr', 'RR', 'N']
+                result = [pc.mean_centers, xi, RR, pc.pair_counts]
+        else:
+            cols = ['r', 'mu', 'corr', 'RR', 'N']
+            r, mu = pc.mean_centers
+            result = [r, mu, xi, RR, pc.pair_counts]
+
+        return pc.edges, dict(zip(cols, result))
+        
+    def save(self, output, result):
+        """
+        Save the result returned by `run()` to the filename specified by `output`
+        
+        Parameters
+        ----------
+        output : str
+            the string specifying the file to save
+        result : tuple
+            the tuple returned by `run()` -- first argument specifies the bin
+            edges and the second is a dictionary holding the data results
+        """
+        from nbodykit.extensionpoints import MeasurementStorage
+        
+        edges, result = result
+        storage = MeasurementStorage.new(self.mode, output)
+        
+        cols = list(result.keys())
+        values = list(result.values())
+        storage.write(edges, cols, values)
+
+            
+
+
+
