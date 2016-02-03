5e3136fa6e792f3d16cd1be3feb93747ee58efaf
Nick Hand <nhand@berkeley.edu>
2016-02-02 17:45:31 -0800

    updating examples to use `nbkit.py PeriodicPower` and `-c` syntax

--- examples/power/run.sh
+++ examples/power/run.sh
@@ -3,10 +3,10 @@ cd $DIR
 [ -d ../output ] || mkdir ../output
 for fn in *.params; do
     echo testing $fn ...
-    mpirun -n 2 python ../../bin/power.py $fn || exit
+    mpirun -n 2 python ../../bin/nbkit.py PeriodicPower -c $fn || exit
 done
 
 for fn in *.argparse; do
     echo testing $fn ...
-    mpirun -n 2 python ../../bin/power.py @$fn || exit
+    mpirun -n 2 python ../../bin/nbkit.py PeriodicPower @$fn || exit
 done
