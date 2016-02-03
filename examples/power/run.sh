4a6b3b92931612e17e5a4fd4beb13baa5a2ad747
Nick Hand <nhand@berkeley.edu>
2016-02-02 18:46:58 -0800

    adding FFTCorrelation to PeriodicPower.py, with one working example

--- examples/power/run.sh
+++ examples/power/run.sh
@@ -3,7 +3,12 @@ cd $DIR
 [ -d ../output ] || mkdir ../output
 for fn in *.params; do
     echo testing $fn ...
-    mpirun -n 2 python ../../bin/nbkit.py PeriodicPower -c $fn || exit
+    if [[ $fn == *"corr"* ]]
+    then
+        mpirun -n 2 python ../../bin/nbkit.py FFTCorrelation -c $fn || exit
+    else
+        mpirun -n 2 python ../../bin/nbkit.py PeriodicPower -c $fn || exit
+    fi
 done
 
 for fn in *.argparse; do
--- nbodykit/plugins/algorithms/PeriodicPower.py
+++ nbodykit/plugins/algorithms/PeriodicPower.py
@@ -180,9 +180,6 @@ class PeriodicPowerAlgorithm(Algorithm):
         dk = 2*numpy.pi/pm.BoxSize.min() if self.dk is None else self.dk
         kedges = numpy.arange(self.kmin, numpy.pi*pm.Nmesh/pm.BoxSize.min() + dk/2, dk)
     
-        # col names
-        x_str, y_str = 'k', 'power'
-    
         # project on to the desired basis
         muedges = numpy.linspace(0, 1, self.Nmu+1, endpoint=True)
         edges = [kedges, muedges]
@@ -223,6 +220,122 @@ class PeriodicPowerAlgorithm(Algorithm):
             else:
                 edges_ = edges
                 cols = ['k', 'mu', 'power', 'modes']
+                
+            # write binned statistic
+            logger.info('measurement done; saving result to %s' %output)
+            storage = MeasurementStorage.new(self.mode, output)
+            storage.write(edges_, cols, result, **meta)
+        
+            # write multipoles
+            if len(self.poles):
+                filename, ext = os.path.splitext(output)
+                pole_output = filename + '_poles' + ext
+            
+                # format is k pole_0, pole_1, ...., modes_1d
+                logger.info('saving ell = %s multipoles to %s' %(",".join(map(str,self.poles)), pole_output))
+                storage = MeasurementStorage.new('1d', pole_output)
+            
+                k, poles, N = pole_result
+                cols = ['k'] + ['power_%d' %l for l in self.poles] + ['modes']
+                pole_result = [k] + [pole for pole in poles] + [N]
+                storage.write(edges[0], cols, pole_result, **meta)
+                
+
+class FFTCorrelationAlgorithm(Algorithm):
+    """
+    Algorithm to compute the 1d or 2d correlation function and multipoles
+    in a periodic box, using an FFT
+    """
+    plugin_name = "FFTCorrelation"
+    
+    def __init__(self, comm, mode, Nmesh, fields, los='z', Nmu=5, dk=None, 
+                    kmin=0., log_level=logging.DEBUG, poles=[]):
+        """
+        The MPI communicator must be the first argument, followed by the parameters
+        specified by the command-line parser
+        """  
+        self.comm      = comm       
+        self.mode      = mode
+        self.Nmesh     = Nmesh
+        self.fields    = fields
+        self.los       = los
+        self.Nmu       = Nmu
+        self.dk        = dk
+        self.kmin      = kmin
+        self.log_level = log_level
+        self.poles     = poles
+
+    @classmethod
+    def register(kls):
+
+        kls.parser = PeriodicPowerAlgorithm.parser
+        kls.parser.description = "correlation spectrum calculator via FFT in a periodic box"
+        kls.parser.prog = 'FFTCorrelation'
+        
+    def run(self):
+        """
+        Run the algorithm, which computes and returns the correlation function
+        """
+        from nbodykit import measurestats
+        from pmesh.particlemesh import ParticleMesh
+        
+        logger.setLevel(self.log_level)
+        if self.comm.rank == 0: logger.info('importing done')
+
+        # setup the particle mesh object, taking BoxSize from the painters
+        pm = ParticleMesh(self.fields[0][0].BoxSize, self.Nmesh, dtype='f4', comm=self.comm)
+
+        # only need one mu bin if 1d case is requested
+        if self.mode == "1d": self.Nmu = 1
+    
+        # measure
+        y3d, N1, N2 = measurestats.compute_3d_corr(self.fields, pm, comm=self.comm, log_level=self.log_level)
+        x3d = pm.x
+                
+        # make the bin edges
+        dr = pm.BoxSize[0] / pm.Nmesh
+        redges = numpy.arange(0, pm.BoxSize[0] + dr * 0.5, dr)
+            
+        # project on to the desired basis
+        muedges = numpy.linspace(0, 1, self.Nmu+1, endpoint=True)
+        edges = [redges, muedges]
+        result, pole_result = measurestats.project_to_basis(pm.comm, x3d, y3d, edges, 
+                                                            poles=self.poles, 
+                                                            los=self.los, 
+                                                            symmetric=False)
+                                                            
+        # compute the metadata to return
+        Lx, Ly, Lz = pm.BoxSize
+        meta = {'Lx':Lx, 'Ly':Ly, 'Lz':Lz, 'volume':Lx*Ly*Lz, 'N1':N1, 'N2':N2}                                                    
+        
+        # return all the necessary results
+        return edges, result, pole_result, meta
+
+    def save(self, output, result):
+        """
+        Save the correlation function results to the specified output file
+        
+        Parameters
+        ----------
+        output : str
+            the string specifying the file to save
+        result : tuple
+            the tuple returned by `run()` -- first argument specifies the bin
+            edges and the second is a dictionary holding the data results
+        """
+        from nbodykit.extensionpoints import MeasurementStorage
+        
+        # only the master rank writes        
+        if self.comm.rank == 0:
+            
+            edges, result, pole_result, meta = result
+            if self.mode == "1d":
+                cols = ['r', 'corr', 'modes']
+                result = [numpy.squeeze(result[i]) for i in [0, 2, 3]]
+                edges_ = edges[0]
+            else:
+                edges_ = edges
+                cols = ['r', 'mu', 'corr', 'modes']
                 
             # write binned statistic
             logger.info('measurement done; saving result to %s' %output)
