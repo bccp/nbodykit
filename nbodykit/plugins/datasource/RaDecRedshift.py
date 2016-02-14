648bfa6e2efa19f757506c12b5279c2969d3b13e
Nick Hand <nhand@berkeley.edu>
2016-02-13 21:14:44 -0800

    RaDecRedshift has to deal with pretty large files (randoms files) -- use bunch size option of pandas.read_csv

--- nbodykit/plugins/datasource/RaDecRedshift.py
+++ nbodykit/plugins/datasource/RaDecRedshift.py
@@ -11,8 +11,8 @@ def list_str(value):
     
 class RaDecRedshiftDataSource(DataSource):
     """
-    DataSource to read in (ra, dec, redshift, weigh) -- only handles 
-    the reading of the files
+    DataSource to read a plaintext file in (ra, dec, redshift, weigh) 
+    -- only handles the reading of the files
     """
     plugin_name = "RaDecRedshift"
             
@@ -40,25 +40,22 @@ class RaDecRedshiftDataSource(DataSource):
             help='if input (ra,dec) are in degrees, set this flag to convert them to radians')
         p.add_argument("-select", default=None, type=selectionlanguage.Query, 
             help='row selection based on conditions specified as string')
-        p.add_argument("-ftype", default='auto', choices=['hdf5', 'text', 'auto'], 
-            help='format of the Pandas storage container. auto is to guess from the file name.')
+        p.add_argument("-bunchsize", type=int, 
+                default=1024*1024*4, help="number of objects to read per rank in a bunch")
                   
-    def readall(self, columns):
+    def read(self, columns, stats, full=False):        
         try:
             import pandas as pd
         except:
             name = self.__class__.__name__
             raise ImportError("pandas must be installed to use %s" %name)
-                
-        if self.ftype == 'auto':
-            if self.path.endswith('.hdf5'):
-                self.ftype = 'hdf5'
-            else: 
-                self.ftype = 'text'
-        if self.ftype == 'hdf5':
-            # read in the hdf5 file using pandas
-            data = pd.read_hdf(self.path, self.names[0], columns=self.usecols)
-        elif self.ftype == 'text':
+        
+        bunchsize = self.bunchsize
+        if full: bunchsize = None
+        
+        stats['Ntot'] = 0.
+        if self.comm.rank == 0:
+                    
             # read in the plain text file using pandas
             kwargs = {}
             kwargs['comment'] = '#'
@@ -67,29 +64,66 @@ class RaDecRedshiftDataSource(DataSource):
             kwargs['engine'] = 'c'
             kwargs['delim_whitespace'] = True
             kwargs['usecols'] = self.usecols
-            data = pd.read_csv(self.path, **kwargs)
-
-        nobj = len(data)
-        # select based on input conditions
-        if self.select is not None:
-            mask = self.select.get_mask(data)
-            data = data[mask]
+            kwargs['chunksize'] = bunchsize
+            data_iter = iter(pd.read_csv(self.path, **kwargs))
         
-        # rescale the angles
-        if self.degrees:
-            data[self.sky_cols] *= numpy.pi/180.
-        
-        # get the (ra, dec, z) coords
-        cols = self.sky_cols + [self.z_col]
-        pos = data[cols].values.astype('f4')
-        
-        # get the weights
-        w = numpy.ones(len(pos))
-        if self.weight_col is not None:
-            w = data[self.weight_col].values.astype('f4')
+        stop = False
+        cols = ['ra', 'dec', 'z']
+        while not stop:
             
-        P = {}
-        P['Position'] = pos
-        P['Weight'] = w
+            if self.comm.rank == 0:
+                
+                try:
+                    data = data_iter.next()
+                    
+                    # select based on input conditions
+                    if self.select is not None:
+                        mask = self.select.get_mask(data)
+                        data = data[mask]
 
-        return [P[key] for key in columns]
+                    # rescale the angles
+                    if self.degrees:
+                        data[self.sky_cols] *= numpy.pi/180.
+
+                    # get the (ra, dec, z) coords
+                    cols = self.sky_cols + [self.z_col]
+                    pos = data[cols].values.astype('f4')
+
+                    # get the weights
+                    w = numpy.ones(len(pos))
+                    if self.weight_col is not None:
+                        w = data[self.weight_col].values.astype('f4')
+
+                    P = {}
+                    P['Position'] = pos
+                    P['Weight'] = w
+
+                    data = [P[key] for key in columns]
+                except StopIteration:
+                    stop = True
+                
+                if not stop:
+                    shape_and_dtype = [(d.shape, d.dtype) for d in data]
+                    Ntot = len(data[0]) # columns has to have length >= 1, or we crashed already
+            
+            else:
+                shape_and_dtype = None
+                Ntot = None
+                
+            # check if we are stopping
+            stop = self.comm.bcast(stop)
+            if stop: break
+                
+            shape_and_dtype = self.comm.bcast(shape_and_dtype)
+            stats['Ntot'] += self.comm.bcast(Ntot)
+
+            if self.comm.rank != 0:
+                data = [
+                    numpy.empty(0, dtype=(dtype, shape[1:]))
+                    for shape,dtype in shape_and_dtype
+                ]
+
+            yield data
+        
+              
+
