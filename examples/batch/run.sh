d740ec69b888e7e1bd61ed5263e7d12c08f4d307
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2016-02-24 15:30:16 -0800

    strip out run-stdin.sh

--- examples/batch/run.sh
+++ examples/batch/run.sh
@@ -5,15 +5,3 @@ cd $DIR
 echo testing nbkit-batch.py ...
 mpirun -np 6 python ../../bin/nbkit-batch.py FFTPower 2 -c test_power_batch.template -i "los: [x, y, z]" --extras extra.template
 
-echo testing nbkit.py from STDIN ...
-mpirun -np 2 python ../../bin/nbkit.py FFTPower <<EOF
-mode: 1d
-Nmesh: 256
-output: ../output/test_power_fastpm_1d.txt
-
-field:
-    DataSource: 
-       plugin: FastPM
-       path: ../data/fastpm_1.0000
-    Transfer: [NormalizeDC, RemoveDC, AnisotropicCIC]
-EOF
