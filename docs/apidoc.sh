7e6ba7558b7be32e3321179231cc10d98f3eb760
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2016-05-12 14:02:33 -0700

    fix docstrings of plugins by appending nbodykit to the full names.

--- docs/apidoc.sh
+++ docs/apidoc.sh
@@ -4,5 +4,13 @@ if ! python -c 'import numpydoc'; then easy_install --user numpydoc; fi
 if ! python -c 'import sphinx'; then easy_install --user sphinx; fi
 
 sphinx-apidoc -H "API Reference" -M -e -f -o api/ ../nbodykit ../nbodykit/plugins
-
-sphinx-apidoc -H "Plugins Reference" -M -e -f -o plugins/ ../nbodykit/plugins
+exclude=__init__.py
+for i in ../nbodykit/*/; do
+    if [ $i == ../nbodykit/plugins/ ]; then continue; fi
+    exclude="$exclude,$i"
+done
+for i in ../nbodykit/*.py; do
+    exclude="$exclude,$i"
+done
+echo $exclude
+sphinx-apidoc -H "Plugins Reference" -M -e -f -o plugins/ ../nbodykit $exclude
