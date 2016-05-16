06268a0454543e1d2659852c85f766b6a3ad8a0f
Nick Hand <nhand@berkeley.edu>
2016-05-16 16:20:17 -0700

    more mock modules...

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,8 @@ import os
 import shlex
 
 import mock
-MOCK_MODULES = ['mpsort', 'mpi4py', 'scipy', 'scipy.interpolate', 'h5py', 'bigfile']
+MOCK_MODULES = ['mpsort', 'mpi4py', 'scipy', 'scipy.interpolate', 'h5py', 'bigfile', 
+                'kdcount', 'pmesh', 'pmesh.particlemesh']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
