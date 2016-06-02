ccb9d04a917dc4beb4c9c6e9f716a981387ed545
Nick Hand <nhand@berkeley.edu>
2016-06-02 17:16:12 -0400

    build either 'latest' or 'stable', based on current git branch

--- nersc/build.sh
+++ nersc/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# parse options
+# print the usage
 while getopts ":h" opt; do
   case ${opt} in
     h )
       echo "usage:"
-      echo "    build.sh -h                      Display this help message."
-      echo "    build.sh all <version>           Build both the source and the dependencies."
-      echo "    build.sh source <version>        Build only the source."
-      echo "    build.sh deps                    Build only the dependencies."
+      echo "    build.sh -h            Display this help message."
+      echo "    build.sh all           Build both the source and the dependencies."
+      echo "    build.sh source        Build only the source."
+      echo "    build.sh deps          Build only the dependencies."
       exit 0
       ;;
    \? )
@@ -19,21 +19,31 @@ while getopts ":h" opt; do
 done
 shift $((OPTIND -1))
 
-subcommand=$1; shift  # Remove build.sh from the argument list
-if [[ "$subcommand" != "deps" && $# != 1 ]]
-then
-    echo "please provide a <version> to build as the 2nd argument"
-    exit 1
-fi
+# get the subcommand and shorten the argument list
+subcommand=$1; shift
 
-version=$1; shift
+# get the current git branch
+curr_branch=$(git rev-parse --abbrev-ref HEAD)
+case "$curr_branch" in 
+    master )
+       version='stable'
+    ;;
+    develop )
+       version='latest'
+    ;;
+    * )
+       echo "when running build.sh, current git branch should be 'master' or 'develop'"
+       exit 1
+    ;;
+esac
+
+# make the build directory
 mkdir -p ${NERSC_HOST}
 
 # activate python-mpi-bcast
 source /usr/common/contrib/bccp/python-mpi-bcast/activate.sh
 
 case "$subcommand" in
-
   all )
     MPICC=cc bundle-pip ${NERSC_HOST}/nbodykit-dep.tar.gz -r ../requirements.txt
     bundle-pip ${NERSC_HOST}/nbodykit-${version}.tar.gz ..
