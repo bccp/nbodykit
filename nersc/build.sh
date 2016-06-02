129c3019249e0b1f589a71cc17dc771872a70a4f
Nick Hand <nhand@berkeley.edu>
2016-06-02 16:42:40 -0400

    only need a version tag when building the source, not dependencies

--- nersc/build.sh
+++ nersc/build.sh
@@ -8,7 +8,7 @@ while getopts ":h" opt; do
       echo "    build.sh -h                      Display this help message."
       echo "    build.sh all <version>           Build both the source and the dependencies."
       echo "    build.sh source <version>        Build only the source."
-      echo "    build.sh deps <version>          Build only the dependencies."
+      echo "    build.sh deps                    Build only the dependencies."
       exit 0
       ;;
    \? )
@@ -20,33 +20,29 @@ done
 shift $((OPTIND -1))
 
 subcommand=$1; shift  # Remove build.sh from the argument list
-if [ $# != 1 ]
+if [[ "$subcommand" != "deps" && $# != 1 ]]
 then
     echo "please provide a <version> to build as the 2nd argument"
     exit 1
 fi
 
 version=$1; shift
+mkdir -p ${NERSC_HOST}
 
 # activate python-mpi-bcast
 source /usr/common/contrib/bccp/python-mpi-bcast/activate.sh
 
 case "$subcommand" in
-  
+
   all )
-    mkdir -p ${NERSC_HOST}/${version}
-    MPICC=cc bundle-pip ${NERSC_HOST}/${version}/nbodykit-dep.tar.gz -r ../requirements.txt 
-    bundle-pip ${NERSC_HOST}/nbodykit.tar.gz ..
+    MPICC=cc bundle-pip ${NERSC_HOST}/nbodykit-dep.tar.gz -r ../requirements.txt
+    bundle-pip ${NERSC_HOST}/nbodykit-${version}.tar.gz ..
   ;;
   source )
-    version=$1; shift
-    mkdir -p ${NERSC_HOST}/${version}
-    bundle-pip ${NERSC_HOST}/nbodykit.tar.gz ..
+    bundle-pip ${NERSC_HOST}/nbodykit-${version}.tar.gz ..
     ;;
   deps )
-    version=$1; shift
-    mkdir -p ${NERSC_HOST}/${version}
-    MPICC=cc bundle-pip ${NERSC_HOST}/${version}/nbodykit-dep.tar.gz -r ../requirements.txt 
+    MPICC=cc bundle-pip ${NERSC_HOST}/nbodykit-dep.tar.gz -r ../requirements.txt
     ;;
    * )
     echo "invalid build choice -- choose from 'source' or 'deps'"
