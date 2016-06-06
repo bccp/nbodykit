19a768fb16561dc668209d6f685216f28c8345e9
Yu Feng <yfeng1@berkeley.edu>
2016-06-06 13:39:57 -0700

    Add a barebone nersc example.

---
+++ nersc/example.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -p debug
+#SBATCH -o nbkit-example
+#SBATCH -n 16
+
+# You can also allocate the nodes with salloc
+#
+# salloc -n 16
+#
+# and type the commands in the shell obtained from salloc
+
+source /usr/common/contrib/bccp/nbodykit/activate.sh
+
+# regular nbodykit command lines
+# replace nbkit.py with srun-nbkit
+
+srun-nbkit -n 16 FFTPower --help
+
+# You can also do this in an interactive shell
+# e.g.
+
