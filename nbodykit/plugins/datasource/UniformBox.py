7f73904b3170701cf5ee55b5e6174487543c5e35
Nick Hand <nhand@berkeley.edu>
2016-06-08 23:48:18 -0400

    add Mass to uniform box and only use a random seed if one is provided

--- nbodykit/plugins/datasource/UniformBox.py
+++ nbodykit/plugins/datasource/UniformBox.py
@@ -6,17 +6,23 @@ logger = logging.getLogger('UniformBox')
          
 class UniformBoxDataSource(DataSource):
     """
-    Class to return particles uniformly distributed
-    in a box between [0, BoxSize], and velocities 
-    uniformly distributed between ``[-max_speed, max_speed]``
+    DataSource to return the following columns:
+    
+    `Position` : uniformly distributed in box
+    `Velocity` : uniformly distributed between ``+/- max_speed``
+    `LogMass`  : normally distributed with mean `mu_logM` and std dev `sigma_logM`
+    `Mass`     : values corresponding to 10**`LogMass`
     """
     plugin_name = "UniformBox"
     
-    def __init__(self, N, BoxSize, max_speed=500., seed=1234):        
+    def __init__(self, N, BoxSize, max_speed=500., mu_logM=13.5, sigma_logM=0.5, seed=None):        
         
         # initalize a random state for each rank
-        seed = self.seed*(self.comm.rank+1)
-        self.random = numpy.random.RandomState(seed)
+        if self.seed is not None:
+            seed = self.seed*(self.comm.rank+1)
+            self.random = numpy.random.RandomState(seed)
+        else:
+            self.random = numpy.random
             
     @classmethod
     def register(cls):
@@ -30,18 +36,28 @@ class UniformBoxDataSource(DataSource):
             help="the size of the isotropic box, or the sizes of the 3 box dimensions")
         s.add_argument("max_speed", type=float, 
             help="use uniform velocities between [-max_speed, max_speed] (in km/s)")
+        s.add_argument("mu_logM", type=float, 
+            help="the mean log10 mass to use when generating mass values")
+        s.add_argument("sigma_logM", type=float, 
+            help="the standard deviation of the log10 mass to use when generating mass values")
         s.add_argument("seed", type=int,
             help='the number used to seed the random number generator')
     
     def readall(self):
         """
-        Return `Position` and `Velocity` distributed normally in the box
+        Valid columns are:
+            `Position` : uniformly distributed in box
+            `Velocity` : uniformly distributed between ``+/- max_speed``
+            `LogMass`  : normally distributed with mean `mu_logM` and std dev `sigma_logM`
+            `Mass`     : values corresponding to 10**`LogMass` 
         """            
         toret = {}
         shape = (self.N, 3)
         
         toret['Position'] = self.random.uniform(size=shape) * self.BoxSize
         toret['Velocity'] = 2*self.max_speed * self.random.uniform(size=shape) - self.max_speed
+        toret['LogMass']  = self.random.normal(loc=self.mu_logM, scale=self.sigma_logM, size=self.N)
+        toret['Mass']     = 10**(toret['LogMass'])
         
         return toret
 
