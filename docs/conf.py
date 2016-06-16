3569453bccacbbda4f48d75d2c3a3ff4f5de52a1
Nick Hand <nhand@berkeley.edu>
2016-06-16 01:00:34 -0700

    continue to test

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ numpydoc_class_members_toctree = False
 ipython_savefig_dir = '_images'
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
