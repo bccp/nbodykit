efa7ae1c5ff5208ea1c3c56d15744ae0ba55816d
Nick Hand <nhand@berkeley.edu>
2016-07-06 14:51:57 -0700

    omit the extern module from documentation

--- docs/apidoc.sh
+++ docs/apidoc.sh
@@ -3,4 +3,4 @@
 if ! python -c 'import numpydoc'; then easy_install --user numpydoc; fi
 if ! python -c 'import sphinx'; then easy_install --user sphinx; fi
 
-sphinx-apidoc -H "API reference" -M -e -f -o api/ ../nbodykit 
+sphinx-apidoc -H "API reference" -M -e -f -o api/ ../nbodykit ../nbodykit/extern
