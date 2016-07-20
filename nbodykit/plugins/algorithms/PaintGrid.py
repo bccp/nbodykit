f0e816c9f2919eed2e43af6851f5d88f4837d091
Yu Feng <rainwoodman@gmail.com>
2016-07-20 13:45:37 -0700

    add PaintGrid algorithm.
    Test cases doesn't work yet.

---
+++ nbodykit/plugins/algorithms/PaintGrid.py
@@ -0,0 +1,96 @@
+from nbodykit.extensionpoints import Algorithm
+from nbodykit.extensionpoints import DataSource, Painter
+import os
+import numpy
+
+class PaintGridAlgorithm(Algorithm):
+    """
+    Algorithm to paint a data source to a 3D configuration space grid.
+
+    Notes
+    -----
+    The algorithm saves the grid to a bigfile File.
+
+    """
+    plugin_name = "PaintGrid"
+    
+    def __init__(self, Nmesh, DataSource, Painter=None, paintbrush='cic'):
+        # combine the two fields
+        self.datasource = DataSource
+        if Painter is None:
+            Painter = Painter.create("DefaultPainter")
+        self.painter = Painter
+
+    @classmethod
+    def register(cls):
+        s = cls.schema
+        s.description = "periodic power spectrum calculator via FFT"
+
+        # required
+        s.add_argument("Nmesh", type=int, 
+            help='the number of cells in the gridded mesh')
+
+        # the first field
+        s.add_argument('DataSource', type=DataSource.from_config, required=True,
+            help="first data field; a tuple of (DataSource, Painter, Transfer)")
+        s.add_argument('Painter', type=Painter.from_config, required=False, 
+            help='the Painter; run `nbkit.py --list-painters` for all options')
+
+        s.add_argument('paintbrush', type=lambda x: x.lower(), choices=['cic', 'tsc'],
+            help='the density assignment kernel to use when painting; '
+                 'CIC (2nd order) or TSC (3rd order)')
+
+    def run(self):
+        """
+        Run the algorithm, which computes and returns the grid
+        """
+        from nbodykit import measurestats
+        from pmesh.particlemesh import ParticleMesh
+
+        if self.comm.rank == 0:
+            self.logger.info('importing done')
+
+        # setup the particle mesh object, taking BoxSize from the painters
+        pm = ParticleMesh(self.datasource.BoxSize, self.Nmesh, 
+                            paintbrush=self.paintbrush, dtype='f4', comm=self.comm)
+
+        self.painter.paint(pm, self.datasource)
+        # return all the necessary results
+        return pm
+
+    def save(self, output, result):
+        """
+        Save the power spectrum results to the specified output file
+        
+        Parameters
+        ----------
+        output : str
+            the string specifying the file to save
+        result : tuple
+            the tuple returned by `run()` -- first argument specifies the bin
+            edges and the second is a dictionary holding the data results
+        """
+        import bigfile
+        import numpy
+        import mpsort
+        pm = result
+        x3d = pm.real
+        istart = pm.partition.local_i_start
+        data = numpy.empty(x3d.size, dtype=[('value', 'f4'), ('ind', 'i8')])
+
+        data['value'] = x3d.flat
+        data['ind'] = 0
+        ind = data['ind'].reshape(x3d.shape)
+        for d in range(3):
+            i = numpy.arange(istart[d], istart[d] + x3d.shape[d])
+            i = i.reshape([-1 if dd == d else 1 for dd in range(3)])
+            ind[...] *= pm.Nmesh
+            ind[...] += i
+
+        mpsort.sort(data, orderby='ind', comm=self.comm)
+        if self.comm.rank == 0:
+            self.logger.info("writing to %s" % output)
+        f = bigfile.BigFileMPI(self.comm, output, create=True)
+        b = f.create_from_array('PaintGrid', data['value'], Nfile=1)
+        b.attrs['ndarray.shape'] = numpy.array([self.Nmesh, self.Nmesh, self.Nmesh], dtype='i8')
+        b.attrs['BoxSize'] = numpy.array(self.datasource.BoxSize, dtype='f8')
