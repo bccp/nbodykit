a197f026f569040e34534717f89e4b6a8cad1270
Yu Feng <rainwoodman@gmail.com>
2016-08-08 15:21:48 -0700

    Move transformations to DefaultPainter.

--- nbodykit/plugins/datasource/BigFileGrid.py
+++ nbodykit/plugins/datasource/BigFileGrid.py
@@ -13,11 +13,10 @@ class BigFileGridSource(GridSource):
     """
     plugin_name = "BigFileGrid"
 
-    def __init__(self, path, dataset, frho=None, normalize=False, fk=None):
+    def __init__(self, path, dataset):
         import bigfile
         f = bigfile.BigFileMPI(self.comm, self.path)
-        self.dataset = dataset
-        self.path = path
+
         with f[self.dataset] as d:
             self.BoxSize = d.attrs['BoxSize']
             self.Nmesh = int(d.attrs['Nmesh'][0])
@@ -39,9 +38,6 @@ class BigFileGridSource(GridSource):
 
         s.add_argument("path", type=str, help="the file path to load the data from")
         s.add_argument("dataset", type=str, help="the file path to load the data from")
-        s.add_argument("frho", type=str, help="A python expresion for transforming the real space density field. variables: rho. example: 1 + (rho - 1)**2")
-        s.add_argument("fk", type=str, help="A python expresion for transforming the fourier space density field. variables: k. example: exp(-(k * 0.5)**2) ")
-        s.add_argument("normalize", type=bool, help="Normalize the field to set mean == 1")
 
     def read(self, pm):
         import bigfile
@@ -51,37 +47,4 @@ class BigFileGridSource(GridSource):
         f = bigfile.BigFileMPI(self.comm, self.path)
         with f[self.dataset] as ds:
             resampler.read(pm, ds, self.Nmesh, self.isfourier)
-        mean = self.comm.allreduce(pm.real.sum(dtype='f8')) / pm.Nmesh ** 3.
-
-        if self.comm.rank == 0:
-            self.logger.info("Mean = %g" % mean)
-
-        if self.normalize:
-            pm.real *= 1. / mean
-            mean = self.comm.allreduce(pm.real.sum(dtype='f8')) / pm.Nmesh ** 3.
-            if self.comm.rank == 0:
-                self.logger.info("Renormalized mean = %g" % mean)
-
-        if self.fk:
-            if self.comm.rank == 0:
-                self.logger.info("applying transformation fk %s" % self.fk)
-
-            def function(rho):
-                return eval(self.frho)
-            pm.r2c()
-            k = (pm.k[0] ** 2 + pm.k[1] ** 2 + pm.k[2] ** 2) ** 0.5
-            pm.complex[...] *= function(k)
-            pm.c2r()
-
-        if self.frho:
-            if self.comm.rank == 0:
-                self.logger.info("applying transformation frho %s" % self.frho)
-
-            def function(rho):
-                return eval(self.frho)
-            if self.comm.rank == 0:
-                self.logger.info("example value before frho %g" % pm.real.flat[0])
-            pm.real[...] = function(pm.real)
-            if self.comm.rank == 0:
-                self.logger.info("example value after frho %g" % pm.real.flat[0])
 
