82bf2ffb926a893b51dfff5c575f818f5fc267bd
Nick Hand <nhand@berkeley.edu>
2016-08-10 21:54:39 -0700

    implementing __getitem__ for BinaryFile

--- nbodykit/dask/binary_file.py
+++ nbodykit/dask/binary_file.py
@@ -2,9 +2,6 @@ from glob import glob
 import os
 import numpy
 from six import string_types
-
-import dask.array as da
-from dask.delayed import delayed
   
 def getsize(filename, header_size, rowsize):
     """
@@ -47,9 +44,9 @@ class BinaryFile(object):
     A file object to handle the reading of columns of data from 
     a binary file
     
-    FIXME: This assumes the chunk size for each column is the size
-    of the binary file. We could also implement the alternative format, 
-    where the chunk size is 1 (traditional "rows" of data)
+    .. warning::
+        
+        This assumes the data is stored in a column-major format
     """
     def __init__(self, path, dtype, header_size=0, peek_size=None):
         """
@@ -92,13 +89,22 @@ class BinaryFile(object):
         
         # header size    
         self.header_size = header_size
-       
+    
+    def __getitem__(self, s):
+        if isinstance(s, tuple): s = s[0]
+        start, stop, step = s.indices(self.size)
+        return self.read(self.keys(), start, stop, step)   
+    
     def __iter__(self):
         return iter(self.keys())
     
     def keys(self):
         return list(self.dtype.names)
 
+    @property
+    def shape(self):
+        return (self.size,)
+        
     @property
     def ncols(self):
         return len(self.dtype)
@@ -163,8 +169,7 @@ class BinaryFile(object):
         'start' and 'stop' should be between 0 and :attr:`size`,
         which is the total size of the binary file (in particles)
         """
-        if isinstance(columns, string_types):
-            columns = [columns]
+        if isinstance(columns, string_types): columns = [columns]
             
         # make the slices positive
         if start < 0: start += self.size
@@ -175,73 +180,38 @@ class BinaryFile(object):
         cumsum[1:] = self.sizes.cumsum()
         fnums = numpy.searchsorted(cumsum[1:], [start, stop])
           
+        # initialize the return array
+        N, remainder = divmod(stop-start, step)
+        if remainder: N += 1
+        toret = numpy.empty(N, dtype=self.dtype)
+          
         # loop over necessary files to get requested range
         global_start = 0
-        toret = numpy.empty((stop-start)//step, dtype=self.dtype)
         for fnum in range(fnums[0], fnums[1]+1):
                         
             # normalize the global start/stop to the per file values     
             start_size = cumsum[fnum] 
             this_slice = (max(start-start_size, 0), min(stop-start_size, self.sizes[fnum]), step)
             
-            # determine the slice of the return array for this chunk
-            diff = (this_slice[1]-this_slice[0]) // step
-            global_slice = slice(global_start, global_start + diff)
-
             # do the reading
             for col in columns:
-                toret[col][global_slice] = self._read_block(col, fnum, *this_slice)
+                tmp = self._read_block(col, fnum, *this_slice)
+                toret[col][global_start:None] = tmp[:]
                 
-            global_start += diff # update where we start slicing return array
+            global_start += len(tmp) # update where we start slicing return array
 
         return toret[columns]
-        
-    def get_partition(self, columns, start, stop, chunksize=None):
-        """
-        Parition the binary file from `start` to `stop`, returning a 
-        list of dask arrays for each sub-chunk in the partition
-        
-        The partition will be chunked according to `chunksize`
-    
-        Parameters
-        ----------
-        columns : str, list of str
-            a string or list of strings specifying the columns to read
-        start : int
-            the start position in particles, ranging from 0 to :attr:`size`
-        stop : int 
-            the stop position in particiles, ranging from 0 to :attr:`size`
-        chunksize : int, optional
-            the number of particles per chunk in axis 0; if `None`, 
-            the full partition is returned
-        """
-        # make sure columns is a list
-        if isinstance(columns, string_types):
-            columns = [columns]
-            
-        # the data type of returning columns
-        dtype = [(name, self.dtype[name].subdtype) for name in self.dtype.names if name in columns]
-        
-        # no chunksize --> return the whole delayed read
-        if chunksize is None:
-            part = delayed(self.read)(columns, start, stop)
-            return [da.from_delayed(part, (stop-start,), dtype)]
-            
-        # get the delayed read function for each partition
-        subparts = []; subpart_sizes = []
-        istart = istop = start
-        while istop < stop:
-            istart = istop; istop = min(istop+chunksize, stop)
-            subpart_sizes.append(istop-istart)
-            subparts.append(delayed(self.read)(columns, istart, istop))
-        
-        # make a dask array for all of the chunks with same size
-        return [da.from_delayed(part, (size,), dtype) for part, size in zip(subparts, subpart_sizes)]
-                            
+                                    
 if __name__ == '__main__':
     
+    import os
+    import dask.array as da
+    
     # file path
-    path = "/global/cscratch1/sd/nhand/Data/RunPBDM/PB00/tpmsph_0.5000.bin.*"
+    if 'cori' in os.environ['HOSTNAME']:
+        path = "/global/cscratch1/sd/nhand/Data/RunPBDM/PB00/tpmsph_0.5000.bin.*"
+    else:
+        path = "/scratch2/scratchdirs/nhand/Data/RunPBDM/PB00/tpmsph_0.5000.bin.*"
     
     # dtype of files
     dtypes = [('Position', ('f4', 3)), ('Velocity', ('f4', 3)), ('ID', 'u8')]
@@ -249,12 +219,12 @@ if __name__ == '__main__':
     # open the binary file
     f = BinaryFile(path, dtypes, header_size=28)
     
-    # get the dask array
-    data = from_binary(f, ['Position', 'ID'], chunksize=31250000)
+    # get the dask array straight from file (this is awesome)
+    data = da.from_array(f, chunks=1024*1024)
     
-    # mean out mean of first 100 particles
-    meanop = data[:100]['Position'].mean(axis=0)
-    print meanop.compute()
+    # mean out mean of first 1000 particles
+    meanop = data[:1000]['Position'].mean(axis=0)
+    print(meanop.compute())
     
             
         
