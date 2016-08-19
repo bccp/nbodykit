465e8a74aea71933d4d0f7c48ca9f6fa6b05d21d
Yu Feng <rainwoodman@gmail.com>
2016-08-18 17:58:01 -0700

    basic support for transformations.

--- nbodykit/core/datasource/MultiFile.py
+++ nbodykit/core/datasource/MultiFile.py
@@ -5,10 +5,11 @@ import numpy
 class MultiFileDataSource(DataSource):
     plugin_name = "MultiFile"
 
-    def __init__(self, filetype, path, args={}):
+    def __init__(self, filetype, path, args={}, transform={}):
         from nbodykit import plugin_manager
         filetype = plugin_manager.get_plugin(filetype)
         self.cat = FileStack(path, filetype, **args)
+        self.transform = transform
 
     @classmethod
     def fill_schema(cls):
@@ -17,10 +18,18 @@ class MultiFileDataSource(DataSource):
         s.add_argument("filetype", help="the file path to load the data from")
         s.add_argument("path", help="the file path to load the data from")
         s.add_argument("args", type=dict, help="the file path to load the data from")
+        s.add_argument("transform", type=dict, help="transformation")
 
     def parallel_read(self, columns, full=False):
         start = self.comm.rank * self.cat.size // self.comm.size
         end = (self.comm.rank  + 1) * self.cat.size // self.comm.size
         # no dask yet!
-        s = self.cat[start:end]
-        yield [s[key] for key in columns]
+        ds = self.cat[start:end]
+        def ev(column):
+            if column in self.transform:
+                g = {'ds' : ds}
+                return eval(self.transform[column], g)
+            else:
+                return ds[column]
+
+        yield [ev(key) for key in columns]
