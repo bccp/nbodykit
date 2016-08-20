c107cc528760e3aa8b8335974c74d4bf69436e6f
Nick Hand <nhand@berkeley.edu>
2016-08-19 17:03:05 -0700

    transfer functions that correct for just the paintbrush window

--- nbodykit/core/transfer/TransferFunction.py
+++ nbodykit/core/transfer/TransferFunction.py
@@ -1,6 +1,52 @@
 from nbodykit.core import Transfer
 import numpy
 
+class TSCWindow(Transfer):
+    """
+    Divide by a kernel in Fourier space to account for
+    the convolution of the gridded quantity with the 
+    CIC window function in configuration space
+    """
+    plugin_name = "TSCWindow"
+    
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def fill_schema(cls):
+        s = cls.schema
+        s.description = "divide by a Fourier-space kernel to account for the TSC gridding window function; "
+        s.description += "see Jing et al 2005 (arxiv:0409240)"
+        
+    def __call__(self, pm, complex):
+        for wi in pm.w:
+            tmp = ( numpy.sin(0.5 * wi) / (0.5 * wi) ) ** 3
+            tmp[wi == 0.] = 1.0
+            complex[:] /= tmp            
+
+class CICWindow(Transfer):
+    """
+    Divide by a kernel in Fourier space to account for
+    the convolution of the gridded quantity with the 
+    CIC window function in configuration space
+    """
+    plugin_name = "CICWindow"
+    
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def fill_schema(cls):
+        s = cls.schema
+        s.description = "divide by a Fourier-space kernel to account for the CIC gridding window function; "
+        s.description += "see Jing et al 2005 (arxiv:0409240)"
+        
+    def __call__(self, pm, complex):
+        for wi in pm.w:
+            tmp = ( numpy.sin(0.5 * wi) / (0.5 * wi) ) ** 2
+            tmp[wi == 0.] = 1.0
+            complex[:] /= tmp
+        
 class AnisotropicCIC(Transfer):
     """
     Divide by a kernel in Fourier space to account for
