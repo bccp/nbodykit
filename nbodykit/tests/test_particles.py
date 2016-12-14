69820fa537d5dfb499a7966100f293ec50a0239c
Yu Feng <rainwoodman@gmail.com>
2016-12-13 17:16:23 -0800

    split zeldovich test to sparse and dense samples.

--- nbodykit/tests/test_particles.py
+++ nbodykit/tests/test_particles.py
@@ -8,11 +8,24 @@ dask.set_options(get=dask.get)
 setup_logging("debug")
 
 @MPITest([1, 4])
-def test_zeldovich(comm):
+def test_zeldovich_sparse(comm):
     cosmo = cosmology.Planck15
     CurrentMPIComm.set(comm)
 
     source = Source.ZeldovichParticles(cosmo, nbar=0.2e-6, redshift=0.55, BoxSize=128., Nmesh=8, rsd=[0, 0, 0], seed=42)
+
+    source.compensated = False
+
+    real = source.paint(mode='real')
+
+    assert_allclose(real.cmean(), 1.0)
+
+@MPITest([1, 4])
+def test_zeldovich_dense(comm):
+    cosmo = cosmology.Planck15
+    CurrentMPIComm.set(comm)
+
+    source = Source.ZeldovichParticles(cosmo, nbar=0.2e-2, redshift=0.55, BoxSize=128., Nmesh=8, rsd=[0, 0, 0], seed=42)
 
     source.compensated = False
 
