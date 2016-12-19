31d4f4b0ad3066fb9732fab820858b73a4260f5a
Nick Hand <nhand@berkeley.edu>
2016-12-19 16:46:11 -0500

    update fftpower test

--- nbodykit/tests/test_fftpower.py
+++ nbodykit/tests/test_fftpower.py
@@ -2,9 +2,8 @@ from mpi4py_test import MPITest
 from nbodykit.lab import *
 from nbodykit.algorithms.fof import FOF, HaloFinder
 from nbodykit import setup_logging
-
-from nbodykit.testing import TestingPowerSpectrum
 from numpy.testing import assert_array_equal
+
 # debug logging
 setup_logging("debug")
 
@@ -47,10 +46,12 @@ def test_fftpower(comm):
 
 @MPITest([1])
 def test_fftpower_mismatch_boxsize(comm):
+    
+    cosmo = cosmology.Planck15
     CurrentMPIComm.set(comm)
+    
     # zeldovich particles
     source1 = Source.UniformParticles(nbar=3e-3, BoxSize=512., seed=42)
-
-    source2 = Source.LinearMesh(TestingPowerSpectrum, BoxSize=1024, Nmesh=32, seed=33)
+    source2 = Source.LinearMesh(cosmology.NoWiggleEHPower(cosmo, 0.55), BoxSize=1024, Nmesh=32, seed=33)
 
     r = FFTPower(source1, second=source2, mode='1d', BoxSize=1024, Nmesh=32)
