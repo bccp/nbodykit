ec5e2e06b0fcf96cb8157ff42dedf7b6a797a051
Nick Hand <nhand@berkeley.edu>
2017-01-07 13:23:21 -0500

    add a transform that creates a constant dask array, limiting memory with strides trick

--- nbodykit/transform.py
+++ nbodykit/transform.py
@@ -9,6 +9,27 @@ def DefaultWeight(source):
     return da.ones(len(source), dtype='f4', chunks=100000)
 
 def SkyToCartesion(ra, dec, redshift, degrees=True, cosmo=None, unit_sphere=False):
+def ConstantArray(value, size, chunks=100000):
+    """
+    Return a dask array of the specified ``size`` holding a 
+    single value
+    
+    This uses numpy's "stride tricks" to avoid replicating 
+    the data in memory for each element of the array.
+    
+    Parameters
+    ----------
+    value : float
+        the scalar value to fill the array with
+    size : int
+        the length of the returned dask array
+    chunks : int; optional
+        the size of the dask array chunks
+    """
+    toret = numpy.array(value) 
+    toret = numpy.lib.stride_tricks.as_strided(toret, (size, toret.size), (0, toret.itemsize))
+    return da.from_array(toret.squeeze(), chunks=chunks)
+    
     """
     Convert sky coordinates (ra, dec, redshift) coordinates to 
     cartesian coordinates, scaled to the comoving distance if `unit_sphere = False`, 
