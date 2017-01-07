d417a7d08ffbf6a0ff6d194f93c7667852ab8359
Nick Hand <nhand@berkeley.edu>
2017-01-07 13:22:05 -0500

    add transform module to lab

--- nbodykit/lab.py
+++ nbodykit/lab.py
@@ -6,3 +6,4 @@ from nbodykit.batch import TaskManager
 from nbodykit import source as Source, cosmology
 from nbodykit.algorithms import *
 from nbodykit import CurrentMPIComm
+from nbodykit import transform
