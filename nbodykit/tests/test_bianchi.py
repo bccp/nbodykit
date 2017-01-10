085560d969359d60e63a18e43fc389ad81934854
Nick Hand <nhand@berkeley.edu>
2017-01-10 11:17:44 -0500

    add a test case illustrating use of RedshiftHistogram in BianchiFFTPower

--- nbodykit/tests/test_bianchi.py
+++ nbodykit/tests/test_bianchi.py
@@ -1,6 +1,8 @@
 from mpi4py_test import MPITest
 from nbodykit.lab import *
 from nbodykit import setup_logging
+
+from scipy.interpolate import InterpolatedUnivariateSpline
 from numpy.testing import assert_allclose
 
 # debug logging
@@ -31,26 +33,70 @@ def test_bianchi(comm):
     
         # constant number density
         s['NZ'] = NBAR
-        
-        # FKP weights
-        P0 = 1e4
-        s['FKPWeight'] = 1.0 / (1 + P0*s['NZ'])
-        
+                
         # completeness weights
-        s['Weight'] = 1.0/s['FKPWeight']**2
+        P0 = 1e4
+        s['Weight'] = (1 + P0*s['NZ'])**2
     
     # the FKP source
     fkp = Source.FKPCatalog(data, randoms)
     fkp = fkp.to_mesh(Nmesh=128, dtype='f8', nbar='NZ', fkp_weight='FKPWeight', comp_weight='Weight')
 
     # compute the multipoles
-    alg = BianchiFFTPower(fkp, max_ell=4, dk=0.005)
+    alg = BianchiFFTPower(fkp, max_ell=4, dk=0.005, use_fkp_weights=True, P0_FKP=P0)
     alg.run()
 
     # tests
     assert_allclose(alg.attrs['data.A'], NDATA*NBAR)
     assert_allclose(alg.attrs['randoms.A'], NDATA*NBAR)
+    
+@MPITest([1, 4])
+def test_with_zhist(comm):
+    
+    NDATA = 1000
+    NBAR = 1e-4
+    FSKY = 0.15
+    
+    CurrentMPIComm.set(comm)
+    cosmo = cosmology.Planck15
+    
+    data = Source.RandomParticles(NDATA, seed=42, use_cache=True)
+    randoms = Source.RandomParticles(NDATA*10, seed=84, use_cache=True)
+    
+    # add the random columns
+    for s in [data, randoms]:
+        
+        # ra, dec, z
+        s['z']   = s.rng.normal(loc=0.5, scale=0.1, size=s.size)
+        s['ra']  = s.rng.uniform(low=110, high=260, size=s.size)
+        s['dec'] = s.rng.uniform(low=-3.6, high=60., size=s.size)
+        
+        # position
+        s['Position'] = transform.SkyToCartesion(s['ra'], s['dec'], s['z'], cosmo=cosmo)
+        
+    # initialize the FKP source
+    fkp = Source.FKPCatalog(data, randoms)
+    
+    # compute NZ from randoms
+    zhist = RedshiftHistogram(fkp.randoms, FSKY, cosmo, redshift='z')
+    zhist.run()
+    
+    # add n(z) from randoms to the FKP source
+    nofz = InterpolatedUnivariateSpline(zhist.bin_centers, zhist.nbar)
+    fkp['randoms.NZ'] = nofz(randoms['z'])
+    fkp['data.NZ'] = nofz(data['z'])
+    
+    # normalize NZ to the total size of the data catalog
+    alpha = 1.0 * data.csize / randoms.csize
+    fkp['randoms.NZ'] *= alpha
+    fkp['data.NZ'] *= alpha
+    
+    # compute the multipoles
+    alg = BianchiFFTPower(fkp.to_mesh(Nmesh=128), max_ell=4, dk=0.005)
+    alg.run()
 
+    assert_allclose(alg.attrs['data.A'], 0.000388338522187, rtol=1e-5)
+    assert_allclose(alg.attrs['randoms.A'], 0.000395808747269, rtol=1e-5) 
 
     
 
