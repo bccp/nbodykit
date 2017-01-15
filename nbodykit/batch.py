ea425873ab4d66e4069c879d01adab63f6a33a4b
Nick Hand <nhand@berkeley.edu>
2017-01-15 11:26:30 -0800

    Implement a collective map() function in TaskManager and add test case
    * the map function can be used in ``emcee`` to do parallelized nbodykit calculations

--- nbodykit/batch.py
+++ nbodykit/batch.py
@@ -153,22 +153,129 @@ class TaskManager(object):
 
         return self
                            
-    def is_master(self):
+    def is_root(self):
         """
-        Is the current process the master?
+        Is the current process the root process?
+        
+        Root is responsible for distributing the tasks to the other available ranks
         """
         return self.rank == 0
 
     def is_worker(self):
         """
-        Is the current process a valid worker (and thus should wait for
-        instructions from the master)
+        Is the current process a valid worker?
+        
+        Workers wait for instructions from the master
         """
-        return self._valid_worker
+        try:
+            return self._valid_worker
+        except:
+            raise ValeuError("workers are only defined when inside the ``with TaskManager()`` context")
 
+    def _get_tasks(self):
+        """
+        Internal generator that yields the next available task from a worker
+        """
+        if self.is_root():
+            raise RuntimeError("Root rank mistakenly told to await tasks")
+            
+        # logging info
+        if self.comm.rank == 0:
+            args = (self.rank, MPI.Get_processor_name(), self.comm.size)
+            self.logger.debug("worker master rank is %d on %s with %d processes available" %args)
+
+        # continously loop and wait for instructions
+        while True:
+            args = None
+            tag = -1
+
+            # have the master rank of the subcomm ask for task and then broadcast
+            if self.comm.rank == 0:
+                self.basecomm.send(None, dest=0, tag=self.tags.READY)
+                args = self.basecomm.recv(source=0, tag=MPI.ANY_TAG, status=self.status)
+                tag = self.status.Get_tag()
+        
+            # bcast to everyone in the worker subcomm
+            args  = self.comm.bcast(args) # args is [task_number, task_value]
+            tag   = self.comm.bcast(tag)
+
+            # yield the task
+            if tag == self.tags.START:
+                
+                # yield the task value
+                yield args
+                
+                # wait for everyone in task group before telling master this task is done
+                self.comm.Barrier() 
+                if self.comm.rank == 0:
+                    self.basecomm.send([args[0], None], dest=0, tag=self.tags.DONE)
+            
+            # see ya later
+            elif tag == self.tags.EXIT:
+                break
+
+        # wait for everyone in task group and exit
+        self.comm.Barrier()
+        if self.comm.rank == 0:
+            self.basecomm.send(None, dest=0, tag=self.tags.EXIT)
+        
+        # debug logging
+        self.logger.debug("rank %d process is done waiting" %self.rank)
+        
+    def _distribute_tasks(self, tasks):
+        """
+        Internal function that distributes the tasks from the root to the workers
+        """
+        if not self.is_root():
+            raise ValueError("only the root rank should distribute the tasks")
+        
+        ntasks = len(tasks)
+        task_index     = 0
+        closed_workers = 0
+        
+        # logging info
+        args = (self.workers, ntasks)
+        self.logger.debug("master starting with %d worker(s) with %d total tasks" %args)
+        
+        # loop until all workers have finished with no more tasks
+        while closed_workers < self.workers:
+        
+            # look for tags from the workers
+            data = self.basecomm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=self.status)
+            source = self.status.Get_source()
+            tag = self.status.Get_tag()
+
+            # worker is ready, so send it a task
+            if tag == self.tags.READY:
+                
+                # still more tasks to compute
+                if task_index < ntasks:
+                    this_task = [task_index, tasks[task_index]]
+                    self.basecomm.send(this_task, dest=source, tag=self.tags.START)
+                    self.logger.debug("sending task `%s` to worker %d" %(str(tasks[task_index]), source))
+                    task_index += 1
+                
+                # all tasks sent -- tell worker to exit
+                else:
+                    self.basecomm.send(None, dest=source, tag=self.tags.EXIT)
+            
+            # store the results from finished tasks
+            elif tag == self.tags.DONE:
+                self.logger.debug("received result from worker %d" %source)
+            
+            # track workers that exited
+            elif tag == self.tags.EXIT:
+                closed_workers += 1
+                self.logger.debug("worker %d has exited, closed workers = %d" %(source, closed_workers))
+                
     def iterate(self, tasks):
         """
         A generator that iterates through a series of tasks in parallel
+        
+        Notes
+        -----
+        This is a collective operation and should be called by 
+        all ranks
         
         Parameters
         ----------
@@ -181,95 +288,66 @@ class TaskManager(object):
         task : 
             the individual items of `tasks`, iterated through in parallel
         """            
-        ntasks = len(tasks)
-    
         # master distributes the tasks and tracks closed workers
-        if self.is_master():
-            
-            task_index     = 0
-            closed_workers = 0
-            
-            # logging info
-            args = (self.workers, ntasks)
-            self.logger.info("master starting with %d worker(s) with %d total tasks" %args)
-            
-            # loop until all workers have finished with no more tasks
-            while closed_workers < self.workers:
-            
-                # look for tags from the workers
-                data = self.basecomm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=self.status)
-                source = self.status.Get_source()
-                tag = self.status.Get_tag()
-
-                # worker is ready, so send it a task
-                if tag == self.tags.READY:
-                    
-                    # still more tasks to compute
-                    if task_index < ntasks:
-                        this_task = [task_index, tasks[task_index]]
-                        self.basecomm.send(this_task, dest=source, tag=self.tags.START)
-                        self.logger.info("sending task `%s` to worker %d" %(str(tasks[task_index]), source))
-                        task_index += 1
-                    
-                    # all tasks sent -- tell worker to exit
-                    else:
-                        self.basecomm.send(None, dest=source, tag=self.tags.EXIT)
+        if self.is_root():
+            self._distribute_tasks(tasks)
+        
+        # workers will wait for instructions           
+        elif self.is_worker():
+            for tasknum, args in self._get_tasks():
+                yield args
                 
-                # store the results from finished tasks
-                elif tag == self.tags.DONE:
-                    self.logger.debug("received result from worker %d" %source)
-                
-                # track workers that exited
-                elif tag == self.tags.EXIT:
-                    closed_workers += 1
-                    self.logger.debug("worker %d has exited, closed workers = %d" %(source, closed_workers))
+    def map(self, function, tasks):
+        """
+        Like the built-in :func:`map` function, apply a function to all
+        of the values in a list and return the list of results.
+        
+        If ``tasks`` contains tuples, the arguments are passed to 
+        ``function`` using the ``*args`` syntax
+        
+        Notes
+        -----
+        This is a collective operation and should be called by 
+        all ranks
+        
+        Parameters
+        ----------
+        function : callable
+            The function to apply to the list.
+        tasks : list
+            The list of tasks
+        
+        Returns
+        -------
+        results : list
+            the list of the return values of :func:`function`
+        """
+        results = []
+        
+        # master distributes the tasks and tracks closed workers
+        if self.is_root():
+            self._distribute_tasks(tasks)
         
         # workers will wait for instructions           
         elif self.is_worker():
             
-            # logging info
-            if self.comm.rank == 0:
-                args = (self.rank, MPI.Get_processor_name(), self.comm.size)
-                self.logger.info("worker master rank is %d on %s with %d processes available" %args)
-
-            # continously loop and wait for instructions
-            while True:
-                args = None
-                tag = -1
-
-                # have the master rank of the subcomm ask for task and then broadcast
-                if self.comm.rank == 0:
-                    self.basecomm.send(None, dest=0, tag=self.tags.READY)
-                    args = self.basecomm.recv(source=0, tag=MPI.ANY_TAG, status=self.status)
-                    tag = self.status.Get_tag()
-            
-                # bcast to everyone in the worker subcomm
-                args  = self.comm.bcast(args) # args is [task_number, task_value]
-                tag   = self.comm.bcast(tag)
-
-                # yield the task
-                if tag == self.tags.START:
-                    
-                    # yield the task value
-                    yield args[1]
-                    
-                    # wait for everyone in task group before telling master this task is done
-                    self.comm.Barrier() 
-                    if self.comm.rank == 0:
-                        self.basecomm.send([args[0], None], dest=0, tag=self.tags.DONE)
+            # iterate through tasks in parallel
+            for tasknum, args in self._get_tasks():
                 
-                # see ya later
-                elif tag == self.tags.EXIT:
-                    break
-
-            # wait for everyone in task group and exit
-            self.comm.Barrier()
-            if self.comm.rank == 0:
-                self.basecomm.send(None, dest=0, tag=self.tags.EXIT)
-            
-            # debug logging
-            self.logger.debug("rank %d process is done waiting" %self.rank)
-
+                # make function arguments consistent with *args
+                if not isinstance(args, tuple):
+                    args = (args,)
+                    
+                # compute the result (only worker root needs to save)
+                result = function(*args)
+                if self.comm.rank == 0:
+                    results.append((tasknum, result))
+                
+        # put the results in the correct order
+        results = self.basecomm.allgather(results)
+        results = [item for sublist in results for item in sublist]
+        return [r[1] for r in sorted(results, key=lambda x: x[0])]
+    
     def __exit__(self, exc_type, exc_value, exc_traceback):
         """
         Exit gracefully by closing and freeing the MPI-related variables
@@ -282,13 +360,12 @@ class TaskManager(object):
             # see https://groups.google.com/forum/embed/#!topic/mpi4py/RovYzJ8qkbc
             os._exit(1)
 
-            
         # wait and exit
         self.logger.debug("rank %d process finished" %self.rank)
         self.basecomm.Barrier()
         
-        if self.is_master():
-            self.logger.info("master is finished; terminating")
+        if self.is_root():
+            self.logger.debug("master is finished; terminating")
 
         CurrentMPIComm.set(self.original_comm)
         
