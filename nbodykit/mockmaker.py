ac2aa200d2388e109eac3f044382e97269c696dc
Nick Hand <nhand@berkeley.edu>
2017-01-19 16:08:08 -0800

    only poisson sample in mockmaker
    we were also moving the position by the ZA displacement befoe

--- nbodykit/mockmaker.py
+++ nbodykit/mockmaker.py
@@ -10,9 +10,9 @@ def gaussian_complex_fields(pm, linear_power, seed, remove_variance=False, compu
     r"""
     Make a Gaussian realization of a overdensity field, :math:`\delta(x)`
     
-    If specified, also compute the corresponding linear Zel'dovich 
-    displacement field :math:`\psi(x)`, which is related to the 
-    linear velocity field via: 
+    If specified, also compute the corresponding 1st order Lagrangian
+    displacement field (Zel'dovich approximation):math:`\psi(x)`, 
+    which is related to the linear velocity field via: 
     
     .. math::
         
@@ -206,17 +206,16 @@ def lognormal_transform(density, bias=1.):
     return toret
     
     
-def poisson_sample_to_points(delta, displacement, pm, nbar, f=0., bias=1., seed=None, comm=None):
+def poisson_sample_to_points(delta, displacement, pm, nbar, bias=1., seed=None, comm=None):
     """
     Poisson sample the linear delta and displacement fields to points. 
     
-    This applies the following steps:
+    The steps in this function:
     
-        1. use a (biased) lognormal transformation to make the overdensity 
-           field positive-definite
-        2. use the Zel'dovich displacement field to mimic nonlinear growth of structure
-        3. poisson sample the overdensity field to points, disributing particles
-           uniformly within the mesh cells
+        1. Apply a biased, lognormal transformation to the input ``delta`` field
+        2. Poisson sample the overdensity field to discrete points
+        3. Disribute the positions of particles uniformly within the mesh cells, 
+           and assign the displacement field at each cell to the particles
     
     Parameters
     ----------
@@ -226,9 +225,6 @@ def poisson_sample_to_points(delta, displacement, pm, nbar, f=0., bias=1., seed=
         the linear displacement fields which is used to move the particles
     nbar : float
         the desired number density of the output catalog of objects
-    f : float, optional
-        the growth rate, equal to `f = dlnD\dlna`, which scales the 
-        strength of the RSD; default is 0. (no RSD)
     bias : float, optional
         apply a linear bias to the overdensity field (default is 1.)
     seed : int, optional
@@ -238,9 +234,9 @@ def poisson_sample_to_points(delta, displacement, pm, nbar, f=0., bias=1., seed=
     -------
     pos : array_like, (N, 3)
         the Cartesian positions of each of the generated particles
-    vel : array_like, (N, 3)
-        the velocity "offsets" of each of the generated particles in the same
-        units as the ``pos`` array
+    displ : array_like, (N, 3)
+        the displacement field sampled for each of the generated particles in the 
+        same units as the ``pos`` array
     """
     if comm is None:
         comm = MPI.COMM_WORLD
@@ -275,10 +271,10 @@ def poisson_sample_to_points(delta, displacement, pm, nbar, f=0., bias=1., seed=
     Ntot = comm.allreduce(Nlocal) # the collective number of particles
     nonzero_cells = N.nonzero() # indices of nonzero cells
 
-    # initialize the mesh of particle positions and velocity
+    # initialize the mesh of particle positions and displacement
     # this has the shape: (number of dimensions, number of nonzero cells)
     pos_mesh = numpy.empty(numpy.shape(nonzero_cells), dtype=delta.dtype)
-    vel_mesh = numpy.empty_like(pos_mesh)
+    disp_mesh = numpy.empty_like(pos_mesh)
     
     # generate the coordinates for each nonzero cell
     for i in range(delta.ndim): 
@@ -286,17 +282,9 @@ def poisson_sample_to_points(delta, displacement, pm, nbar, f=0., bias=1., seed=
         # particle positions initially on the coordinate grid
         pos_mesh[i] = numpy.squeeze(delta.pm.x[i])[nonzero_cells[i]]
         
-        # velocity offsets for each particle
-        vel_mesh[i] = displacement[i][nonzero_cells]
-        
-        # displace the positions by the velocity offset
-        pos_mesh[i] += vel_mesh[i]
-        
-    # convert displacement field to RSD
-    # in Zel'dovich approx, RSD is implemented with an additional factor of (1+f)
-    if f <= 0.:
-        raise ValueError("a RSD direction was provided, but the growth rate is not positive")
-    
+        # displacements for each particle
+        disp_mesh[i] = displacement[i][nonzero_cells]
+                    
     # rank 0 computes the in-cell uniform offsets
     if comm.rank == 0:
         in_cell_shift = numpy.empty((Ntot, delta.ndim), dtype=delta.dtype)
@@ -309,20 +297,19 @@ def poisson_sample_to_points(delta, displacement, pm, nbar, f=0., bias=1., seed=
     counts = comm.allgather(Nlocal) 
     in_cell_shift = ScatterArray(in_cell_shift, comm, root=0, counts=counts)
     
-    # initialize the output array of particle positions and velocity
+    # initialize the output array of particle positions and displacement
     # this has shape: (local number of particles, number of dimensions)
     pos = numpy.zeros((Nlocal, delta.ndim), dtype=delta.dtype)
-    vel = numpy.zeros_like(pos)
+    disp = numpy.zeros_like(pos)
     
     # coordinates of each object (placed randomly in each cell)
     for i in range(delta.ndim):
         pos[:,i] = numpy.repeat(pos_mesh[i], N[nonzero_cells]) + in_cell_shift[:,i]
         pos[:,i] %= delta.BoxSize[i]
 
-    # velocities of each object
+    # displacements of each object
     for i in range(delta.ndim):
-        vel_mesh[i] *= (1. + f) # scale to redshift with extra factor of f
-        vel[:,i] = numpy.repeat(vel_mesh[i], N[nonzero_cells])
+        disp[:,i] = numpy.repeat(disp_mesh[i], N[nonzero_cells])
 
-    return pos, vel
+    return pos, disp
   
