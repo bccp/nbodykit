276c5db94803a9ce1359dde731f42db0be9d5533
Yu Feng <rainwoodman@gmail.com>
2017-01-20 13:13:34 -0800

    Remove FastPM. It is made into a seperate module.

--- nbodykit/source/particle/__init__.py
+++ nbodykit/source/particle/__init__.py
@@ -2,6 +2,5 @@ from .from_file import File
 from .from_numpy import Array
 from .zeldovich import ZeldovichParticles
 from .uniform import UniformParticles, RandomParticles
-from .fastpm import LPTParticles
 from .fkp import FKPCatalog
 
