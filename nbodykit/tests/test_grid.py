f22b78fd3c02cd3eb783b29fa865ec78dfa09c35
Yu Feng <rainwoodman@gmail.com>
2017-01-30 16:48:27 -0800

    Add an adapter to use pmesh Field objects as sources.
    This is useful to integrate nbodykit's FFTPower into the reconstruction
    pipeline.

--- nbodykit/tests/test_grid.py
+++ nbodykit/tests/test_grid.py
@@ -32,6 +32,23 @@ def test_preview(comm):
     assert_allclose(preview.sum(), real.csum(), rtol=1e-5)
 
 @MPITest([1,4])
+def test_memory_grid(comm):
+    from pmesh.pm import ParticleMesh
+
+    pm = ParticleMesh(Nmesh=(8, 8, 8), BoxSize=(128, 128, 128.), comm=comm)
+    real = pm.generate_whitenoise(mode='real', seed=3333)
+    complex = real.r2c()
+
+    realmesh = Source.MemoryMesh(real)
+    complexmesh = Source.MemoryMesh(complex)
+
+    assert_array_equal(real, realmesh.to_field(mode='real'))
+    assert_array_equal(complex, complexmesh.to_field(mode='complex'))
+
+    assert_allclose(complex, realmesh.to_field(mode='complex'))
+    assert_allclose(real, complexmesh.to_field(mode='real'))
+
+@MPITest([1,4])
 def test_linear_grid(comm):
     """
     Compute the power spectrum of a linear density grid and check
