a4c6f64c82ff6cab8bdda5447b8ef0fca984965d
Nick Hand <nhand@berkeley.edu>
2017-03-24 17:01:04 -0700

    add tests of the FileType base class

---
+++ nbodykit/io/tests/test_base.py
@@ -0,0 +1,93 @@
+from nbodykit.io.csv import CSVFile
+import os
+import numpy
+import tempfile
+
+def test_dask():
+
+    with tempfile.NamedTemporaryFile() as ff:    
+        
+        # generate data
+        data = numpy.random.random(size=(100,5))
+        numpy.savetxt(ff, data, fmt='%.7e'); ff.seek(0)
+        
+        # read
+        names =['a', 'b', 'c', 'd', 'e']
+        f = CSVFile(path=ff.name, names=names, blocksize=100)
+        
+        a_dask = f.get_dask('a')
+        numpy.testing.assert_almost_equal(a_dask.compute(), data[:,0])
+
+def test_getitem():
+
+    with tempfile.NamedTemporaryFile() as ff:    
+        
+        # generate data
+        data = numpy.random.random(size=(100,5))
+        numpy.savetxt(ff, data, fmt='%.7e'); ff.seek(0)
+        
+        # read
+        names =['a', 'b', 'c', 'd', 'e']
+        f = CSVFile(path=ff.name, names=names, blocksize=100)
+
+        # empty slice
+        try: empty = f[[]]
+        except IndexError: pass
+        
+        # cannot slice twice
+        a = f['a']
+        try: a2 = a['a']
+        except IndexError: pass
+        
+        # bad column name
+        try: bad = f[['BAD1', 'BAD2']]
+        except IndexError: pass
+        
+        # slice multiple columns
+        f2 = f[['a', 'b']]
+        assert f2.columns == ['a', 'b']
+        f3 = f2[['a']]
+        assert f3.columns == ['a']
+        
+        # slice as an array 
+        d = f.asarray()
+        numpy.testing.assert_almost_equal(d[:,0:2], data[:,0:2])
+        numpy.testing.assert_almost_equal(d[:50], data[:50])
+        numpy.testing.assert_almost_equal(d[0].squeeze(), data[0])
+        
+        # pass list of integers
+        numpy.testing.assert_almost_equal(d[[0,1,2]], data[[0,1,2]])
+        
+        # pass boolean slice
+        valid = numpy.random.choice([True, False], replace=True, size=len(f))
+        f2 = f[valid]
+        numpy.testing.assert_almost_equal(f2['a'][:], data[valid, 0])
+        
+        # wrong slice shape
+        try: d2 = d[:,:,:]
+        except IndexError: pass
+        
+def test_asarray():
+
+    with tempfile.NamedTemporaryFile() as ff:    
+        
+        # generate data
+        data = numpy.random.random(size=(100,5))
+        numpy.savetxt(ff, data, fmt='%.7e'); ff.seek(0)
+        
+        # read
+        names =['a', 'b', 'c', 'd', 'e']
+        f = CSVFile(path=ff.name, names=names, dtype={'a':'f4', 'b':'f8'}, blocksize=100)
+        
+        # cannot do asarray twice
+        a = f['a']
+        try: a2 = a.asarray()
+        except ValueError: pass
+        
+        # cannot do view with different dtypes
+        try: f2 = f.asarray()
+        except ValueError: pass
+        
+        
+        
+        
