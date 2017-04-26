bc5ecf17b4aa7f14c164479ad972dd43c2f8ed9c
Nick Hand <nhand@berkeley.edu>
2017-04-26 10:29:09 -0700

    use a default set of units for cosmology classes; return attributes as Quantity.value (no units)

--- nbodykit/cosmology/core.py
+++ nbodykit/cosmology/core.py
@@ -3,16 +3,29 @@ from scipy.integrate import quad
 import numpy as np
 import functools
 
+def removeunits(f):
+    """
+    Decorator to remove units from :class:`astropy.units.Quantity`
+    instances
+    """
+    @functools.wraps(f)
+    def wrapped(*args, **kwargs):
+        ans = f(*args, **kwargs)
+        if isinstance(ans, units.Quantity):
+            ans = ans.value
+        return ans
+    return wrapped
+
 class fittable(object):
     """
     A "fittable" function
-    
+
     There exists a `.fit()` method of the original function
-    which returns a spline-interpolated version of the function 
+    which returns a spline-interpolated version of the function
     for a specified variable
     """
     def __init__(self, func, instance=None):
-        
+
         # update the docstring, etc from the original func
         functools.update_wrapper(self, func)
         self.func = func
@@ -39,11 +52,11 @@ class fittable(object):
         kwargs : dict; optional
             dict of keywords to pass to the original function
         bins : int, iterable; optional
-            either an iterable specifying the bin edges, or an 
+            either an iterable specifying the bin edges, or an
             integer specifying the number of linearly-spaced bins
         range : tuple; optional
             the range to fit over if `bins` specifies an integer
-        
+
         Returns
         -------
         spl : callable
@@ -51,7 +64,7 @@ class fittable(object):
         """
         from scipy import interpolate
         from astropy.units import Quantity
-        
+
         if isiterable(bins):
             bin_edges = np.asarray(bins)
         else:
@@ -74,7 +87,7 @@ class fittable(object):
 
 def vectorize_if_needed(func, *x):
     """
-    Helper function to vectorize functions on array inputs; 
+    Helper function to vectorize functions on array inputs;
     borrowed from :mod:`astropy.cosmology.core`
     """
     if any(map(isiterable, x)):
@@ -98,23 +111,36 @@ class Cosmology(dict):
     An extension of the :mod:`astropy.cosmology` framework that can
     store additional, orthogonal parameters and behaves like a read-only
     dictionary
-    
+
     The class relies on :mod:`astropy.cosmology` as the underlying
     "engine" for calculation of cosmological quantities. This "engine"
-    is stored as :attr:`engine` and supports :class:`~astropy.cosmology.LambdaCDM` 
+    is stored as :attr:`engine` and supports :class:`~astropy.cosmology.LambdaCDM`
     and :class:`~astropy.cosmology.wCDM`, and their flat equivalents
-    
+
     Any attributes or functions of the underlying astropy engine
     can be directly accessed as attributes or keys of this class
-    
+
+    ..note::
+
+    A default set of units is assumed, so attributes stored internally
+    as :class:`astropy.units.Quantity` instances will be returned
+    here as numpy arrays. Those units are:
+
+        - temperature: ``K``
+        - distance: ``Mpc``
+        - density: ``g/cm^3``
+        - neutrino mass: ``eV``
+        - time: ``Gyr``
+        - H0: ``Mpc/km/s``
+
     .. warning::
-    
-    This class does not currently support a non-constant dark energy 
-    equation of state
-    """  
-    def __init__(self, H0=67.6, Om0=0.31, Ob0=0.0486, Ode0=0.69, w0=-1., Tcmb0=2.7255, 
+
+        This class does not currently support a non-constant dark energy
+        equation of state
+    """
+    def __init__(self, H0=67.6, Om0=0.31, Ob0=0.0486, Ode0=0.69, w0=-1., Tcmb0=2.7255,
                     Neff=3.04, m_nu=0., flat=False, name=None, **kwargs):
-                    
+
         """
         Parameters
         ----------
@@ -138,79 +164,84 @@ class Cosmology(dict):
             if `True`, automatically set `Ode0` such that `Ok0` is zero
         name : str
             a name for the cosmology
-        kwargs : 
+        kwargs :
             additional key/value pairs to store in the dictionary
         """
         # convert neutrino mass to a astropy `Quantity`
         if m_nu is not None:
             m_nu = units.Quantity(m_nu, 'eV')
-        
+
         # the astropy keywords
         kws = {'name':name, 'Ob0':Ob0, 'w0':w0, 'Tcmb0':Tcmb0, 'Neff':Neff, 'm_nu':m_nu, 'Ode0':Ode0}
-        
+
         # determine the astropy class
         if w0 == -1.0: # cosmological constant
             cls = 'LambdaCDM'
             kws.pop('w0')
         else:
             cls = 'wCDM'
-        
+
         # use special flat case if Ok0 = 0
-        if flat: 
+        if flat:
             cls = 'Flat' + cls
             kws.pop('Ode0')
-        
+
         # initialize the astropy engine
         self.engine = getattr(cosmology, cls)(H0=H0, Om0=Om0, **kws)
-        
+
         # add valid params to the underlying dict
         for k in kws:
             if hasattr(self.engine, k):
                 kwargs[k] = getattr(self.engine, k)
         dict.__init__(self, H0=H0, Om0=Om0, **kwargs)
-        
+
         # store D_z normalization
         integrand = lambda a: a ** (-3) * self.engine.inv_efunc(1/a-1.) ** 3
         self._Dz_norm = 1. / quad(integrand, 0., 1. )[0]
-    
+
     def __dir__(self):
         """
-        Explicitly the underlying astropy engine's attributes as 
+        Explicitly the underlying astropy engine's attributes as
         part of the attributes of this class
         """
         this_attrs = set(dict.__dir__(self)) | set(self.keys())
         engine_attrs = set(self.engine.__dir__())
         return list(this_attrs|engine_attrs)
-        
+
     @classmethod
     def from_astropy(self, cosmo, **kwargs):
         """
         Return a :class:`Cosmology` instance from an astropy cosmology
-        
+
         Parameters
         ----------
         cosmo : subclass of :class:`astropy.cosmology.FLRW`
             the astropy cosmology instance
-        ** kwargs : 
+        ** kwargs :
             extra key/value parameters to store in the dictionary
-        """        
-        valid = ['H0', 'Om0', 'Ob0', 'Ode0', 'w0', 'Tcmb0', 'Neff', 'm_nu'] 
+        """
+        valid = ['H0', 'Om0', 'Ob0', 'Ode0', 'w0', 'Tcmb0', 'Neff', 'm_nu']
         for name in valid:
             if hasattr(cosmo, name):
                 kwargs[name] = getattr(cosmo, name)
-        kwargs['flat'] = cosmo.Ok0 == 0.  
+        kwargs['flat'] = cosmo.Ok0 == 0.
         kwargs.setdefault('name', getattr(cosmo, 'name', None))
-        
+
         toret = Cosmology(**kwargs)
         toret.__doc__ += "\n" + cosmo.__doc__
         return toret
-    
+
     def __setitem__(self, key, value):
         """
         No setting --> read-only
         """
         raise ValueError("Cosmology is a read-only dictionary; see clone() to create a copy with changes")
-            
+
+    @removeunits
+    def __getitem__(self, key):
+        return dict.__getitem__(self, key)
+
+    @removeunits
     def __missing__(self, key):
         """
         Missing dict keys returned only if they are attributes of the
@@ -221,32 +252,34 @@ class Cosmology(dict):
             toret = getattr(self.engine, key)
             if not callable(toret):
                 return toret
-        
+
         # otherwise fail
         raise KeyError("no such parameter '%s' in Cosmology" %key)
-    
+
+    @removeunits
     def __getattr__(self, key):
         """
-        Try to return attributes from the underlying astropy engine and 
+        Try to return attributes from the underlying astropy engine and
         then from the dictionary
-        
+
         Notes
         -----
-        For callable attributes part of the astropy engine's public API (i.e., 
+        For callable attributes part of the astropy engine's public API (i.e.,
         functions that do not begin with a '_'), the function will be decorated
-        with the :class:`fittable` class 
+        with the :class:`fittable` class
         """
         try:
             toret = getattr(self.engine, key)
             # if a callable function part of public API of the "engine", make it fittable
             if callable(toret) and not key.startswith('_'):
                 toret = fittable(toret.__func__, instance=self)
+
             return toret
         except:
             if key in self:
                 return self[key]
             raise AttributeError("no such attribute '%s'" %key)
-        
+
     def clone(self, **kwargs):
         """
         Returns a copy of this object, potentially with some changes.
@@ -255,7 +288,7 @@ class Cosmology(dict):
         -------
         newcos : Subclass of FLRW
         A new instance of this class with the specified changes.
-        
+
         Notes
         -----
         This assumes that the values of all constructor arguments
@@ -277,18 +310,18 @@ class Cosmology(dict):
         # filter out astropy-defined parameters and extras
         extras = {k:self[k] for k in self if not hasattr(self.engine, k)}
         extras.update({k:kwargs.pop(k) for k in list(kwargs) if not hasattr(self.engine, k)})
-        
+
         # make the new astropy instance
         new_engine = self.engine.clone(**kwargs)
-        
+
         # return a new Cosmology instance
         return self.from_astropy(new_engine, **extras)
 
     def efunc_prime(self, z):
         """
         Function giving the derivative of :func:`efunc with respect
-        to the scale factor ``a`` 
-        
+        to the scale factor ``a``
+
         Parameters
         ----------
         z : array-like
@@ -299,19 +332,19 @@ class Cosmology(dict):
         eprime : ndarray, or float if input scalar
             The derivative of the hubble factor redshift-scaling with respect
             to scale factor
-        """ 
+        """
         if not np.all(self.de_density_scale(z)==1.0):
             raise NotImplementedError("non-constant dark energy redshift dependence is not supported")
-            
-        if isiterable(z): 
+
+        if isiterable(z):
             z = np.asarray(z)
         zp1 = 1.0 + z
-        
+
         # compute derivative of Or term wrt to scale factor
         if self.has_massive_nu:
             Or = self.Ogamma0 * (1 + self.nu_relative_density(z))
-            
-            # compute derivative of nu_relative_density() function with 
+
+            # compute derivative of nu_relative_density() function with
             # uses fitting formula from Komatsu et al 2011
             p = 1.83
             invp = 0.54644808743  # 1.0 / p
@@ -321,30 +354,30 @@ class Cosmology(dict):
             drel_mass_per =  x / curr_nu_y * (1.0 + x) ** (invp-1) * self._nu_y
             drel_mass = drel_mass_per.sum(-1) + self._nmasslessnu
             nu_relative_density_deriv = 0.22710731766 * self._neff_per_nu * drel_mass
-            
+
             rad_deriv = -4*Or*zp1**5 + zp1**4*self.Ogamma0*nu_relative_density_deriv
         else:
             Or = self.Ogamma0 + self.Onu0
-            rad_deriv = -4*Or*zp1**5 
-            
+            rad_deriv = -4*Or*zp1**5
+
         # dE^2 / da (assumes Ode0 independent of a)
         esq_prime = rad_deriv - 3*self.Om0*zp1**4 - 2*self.Ok0*zp1**3
-        
+
         # dE/dA
         eprime = esq_prime / (2*self.efunc(z))
         return eprime
-    
+
     @fittable
     def growth_rate(self, z):
         """
         Linear growth rate :math:`f(z) = dlnD / dlna`, where ``a`` is the
         scale factor and ``D`` is the growth function, given by :func:`D_z`
-        
+
         Parameters
         ----------
         z : array-like
             Input redshifts.
-        
+
         Returns
         -------
         fz : ndarray, or float if input scalar
@@ -353,41 +386,40 @@ class Cosmology(dict):
         z = np.asarray(z)
         a = 1./(1+z)
         inv_efunc = self.inv_efunc(z)
-        
+
         # D_z integrand
         integrand = lambda red: quad(lambda a: a ** (-3) * self.engine.inv_efunc(1/a-1.) ** 3, 0, 1./(1+red))[0]
         D_z = vectorize_if_needed(integrand, z)
-        
+
         return a * inv_efunc * self.efunc_prime(z) + inv_efunc**3 / (a**2 * D_z)
-    
+
     @fittable
     def growth_function(self, z):
         """
-        Linear growth function :math:`D(z)` at redshift ``z`` 
+        Linear growth function :math:`D(z)` at redshift ``z``
 
         .. math::
-        
+
           D(a) \propto H(a) \int_0^a \frac{da'}{a'^3 H(a')^3}
 
         The normalization is such that :math:`D_1(a=1) = D_1(z=0) = 1`
-        
+
         Parameters
         ----------
         z : array-like
             Input redshifts.
-        
+
         Returns
         -------
         Dz : ndarray, or float if input scalar
             The linear growth function evaluated at the input redshifts
-        """        
+        """
         # this is 1 / (E(a) * a)**3, with H(a) = H0 * E(a)
         integrand = lambda a: a ** (-3) * self.engine.inv_efunc(1/a-1.) ** 3
-       
+
         # normalize to D(z=0) = 1
         norm = self.engine.efunc(z) * self._Dz_norm
-        
+
         # be sure to return vectorized quantities
         f = lambda red: quad(integrand, 0., 1./(1+red))[0]
         return norm * vectorize_if_needed(f, z)
-
--- nbodykit/cosmology/ehpower.py
+++ nbodykit/cosmology/ehpower.py
@@ -23,19 +23,19 @@ class LinearPowerBase(object):
         if isinstance(cosmo, FLRW):
             raise ValueError("please provide a nbodykit.cosmology.Cosmology class; see Cosmology.from_astropy")
         self.cosmo = cosmo
-        
+
         # check required parameters
         required = ['Ob0', 'sigma8', 'n_s']
         for param in required:
             if param not in self.cosmo:
                 raise ValueError("'%s' attribute of cosmology must be provided" %param)
-                
+
         # set sigma8 to the cosmology value
         self._sigma8 = self._sigma8_0 = self.cosmo.sigma8
-        
+
         # redshift
         self.redshift = redshift
-                    
+
     @property
     def attrs(self):
         """
@@ -43,36 +43,31 @@ class LinearPowerBase(object):
         """
         from astropy.units import Quantity
         attrs = {'cosmo.%s' %k : self.cosmo[k] for k in self.cosmo}
-        
-        # get the value for any parameters with units
-        for k in attrs:
-            if isinstance(attrs[k], Quantity):
-                attrs[k] = attrs[k].value
-                
+
         # current value of sigma8
         attrs['cosmo.sigma8'] = self.sigma8
-        
+
         return attrs
-        
+
     @abc.abstractmethod
     def transfer(self, k):
         """
         Subclasses should override this to return the transfer function
         """
         return NotImplemented
-    
+
     @property
     def sigma8(self):
         """
         The present day value of ``sigma_r(r=8 Mpc/h)``, used to normalize
         the power spectrum, which is proportional to the square of this value.
-        
+
         The power spectrum can re-normalized by setting a different
         value for this parameter
-    
+
         """
         return self._sigma8
-        
+
     @sigma8.setter
     def sigma8(self, value):
         """
@@ -80,7 +75,7 @@ class LinearPowerBase(object):
         """
         self._sigma8 = value
         self._normalize()
-        
+
     def _normalize(self):
         """
         Internal function that is called when ``sigma8`` is set, ensuring
@@ -89,23 +84,23 @@ class LinearPowerBase(object):
         """
         # set to unity so we get the un-normalized sigma
         self._norm = 1.0
-        
+
         # power is proportional so square of sigma8
         self._norm = (self.sigma8/self.sigma_r(8.))**2
-    
+
     def __call__(self, k, no_BAO=False):
         """
         Return the linear power spectrum in units of [Mpc/h]^3
         at the redshift :attr:`z`
-        
+
         The amplitude scales with redshift through the square of the
         growth function, evaluated using ``cosmo.growth_function``
-        
+
         Paramters
         ---------
         k : float, array_like
             the wavenumbers in units of h/Mpc
-        
+
         Returns
         -------
         Plin : float, array_like
@@ -121,40 +116,40 @@ class LinearPowerBase(object):
         r = np.asarray(r)
         r[k == 0] = 1.
         return r
-            
+
     def sigma_r(self, r):
         """
-        The mass fluctuation within a sphere of radius ``R``, in 
+        The mass fluctuation within a sphere of radius ``R``, in
         units of Mpc/h
-        
+
         The value of this function with ``R=8`` returns
         :attr:`sigma8`, within numerical precision
-        
+
         Parameters
         ----------
         r : float
-            the scale to compute the mass fluctation over, in 
+            the scale to compute the mass fluctation over, in
             units of `Mpc/h`
         """
         if not hasattr(self, '_norm'): self._normalize()
-        
+
         def integrand(lnk):
             k = np.exp(lnk)
             kr = k*r
             j1_kr = np.sin(kr)/kr**3 - np.cos(kr)/kr**2
             T = self.transfer(k)
             return k*k*k * (k ** self.cosmo.n_s) * (T ** 2) * (3*j1_kr) ** 2
-            
+
         norm = self._norm * (self.sigma8 / self._sigma8_0)**2
         lnk = np.log(np.logspace(-4, 4, 4096))
         sigma_sq = norm*simps(integrand(lnk), x=lnk) / (2*np.pi**2)
         return sigma_sq**0.5
-    
-    
+
+
 class EHPower(LinearPowerBase):
     """
     Eisenstein & Hu (1998) fitting function with BAO wiggles
-    
+
     From EH 1998, Eqs. 26,28-31.
     """
     def __init__(self, cosmo, redshift):
@@ -169,7 +164,7 @@ class EHPower(LinearPowerBase):
         """
         LinearPowerBase.__init__(self, cosmo, redshift)
         self._set_params()
-        
+
     def _set_params(self):
         """
         Initialize the parameters of the fitting formula
@@ -177,48 +172,48 @@ class EHPower(LinearPowerBase):
         self.Obh2      = self.cosmo.Ob0 * self.cosmo.h ** 2
         self.Omh2      = self.cosmo.Om0 * self.cosmo.h ** 2
         self.f_baryon  = self.cosmo.Ob0 / self.cosmo.Om0
-        self.theta_cmb = self.cosmo.Tcmb0.value / 2.7
-    
+        self.theta_cmb = self.cosmo.Tcmb0 / 2.7
+
         # redshift and wavenumber of equality
         self.z_eq = 2.5e4 * self.Omh2 * self.theta_cmb ** (-4) # this is 1 + z
         self.k_eq = 0.0746 * self.Omh2 * self.theta_cmb ** (-2) # units of 1/Mpc
-    
+
         # sound horizon and k_silk
         self.z_drag_b1 = 0.313 * self.Omh2 ** -0.419 * (1 + 0.607 * self.Omh2 ** 0.674)
         self.z_drag_b2 = 0.238 * self.Omh2 ** 0.223
         self.z_drag    = 1291 * self.Omh2 ** 0.251 / (1. + 0.659 * self.Omh2 ** 0.828) * \
                            (1. + self.z_drag_b1 * self.Obh2 ** self.z_drag_b2)
-                        
+
         self.r_drag = 31.5 * self.Obh2 * self.theta_cmb ** -4 * (1000. / (1+self.z_drag))
         self.r_eq   = 31.5 * self.Obh2 * self.theta_cmb ** -4 * (1000. / self.z_eq)
-        
+
         self.sound_horizon = 2. / (3.*self.k_eq) * np.sqrt(6. / self.r_eq) * \
                     np.log((np.sqrt(1 + self.r_drag) + np.sqrt(self.r_drag + self.r_eq)) / (1 + np.sqrt(self.r_eq)) )
         self.k_silk = 1.6 * self.Obh2 ** 0.52 * self.Omh2 ** 0.73 * (1 + (10.4*self.Omh2) ** -0.95)
-    
+
         # alpha_c
         alpha_c_a1 = (46.9*self.Omh2) ** 0.670 * (1 + (32.1*self.Omh2) ** -0.532)
         alpha_c_a2 = (12.0*self.Omh2) ** 0.424 * (1 + (45.0*self.Omh2) ** -0.582)
-        self.alpha_c = alpha_c_a1 ** -self.f_baryon * alpha_c_a2 ** (-self.f_baryon**3) 
-    
+        self.alpha_c = alpha_c_a1 ** -self.f_baryon * alpha_c_a2 ** (-self.f_baryon**3)
+
         # beta_c
         beta_c_b1 = 0.944 / (1 + (458*self.Omh2) ** -0.708)
         beta_c_b2 = 0.395 * self.Omh2 ** -0.0266
         self.beta_c = 1. / (1 + beta_c_b1 * ((1-self.f_baryon) ** beta_c_b2) - 1)
-    
+
         y = self.z_eq / (1 + self.z_drag)
         alpha_b_G = y * (-6.*np.sqrt(1+y) + (2. + 3.*y) * np.log((np.sqrt(1+y)+1) / (np.sqrt(1+y)-1)))
         self.alpha_b = 2.07 *  self.k_eq * self.sound_horizon * (1+self.r_drag)**-0.75 * alpha_b_G
-        
+
         self.beta_node = 8.41 * self.Omh2 ** 0.435
         self.beta_b    = 0.5 + self.f_baryon + (3. - 2.*self.f_baryon) * np.sqrt( (17.2*self.Omh2) ** 2 + 1 )
-        
+
     def transfer(self, k):
         """
         Return the transfer function with BAO wiggles
-        
+
         This is normalized to unity on large scales
-        
+
         Paramters
         ---------
         k : float, array_like
@@ -226,11 +221,11 @@ class EHPower(LinearPowerBase):
         """
         if np.isscalar(k) and k == 0.:
             return 1.0
-        
+
         k = np.asarray(k)
         # only compute k > 0 modes
         valid = k > 0.
-        
+
         k = k[valid] * self.cosmo.h # now in 1/Mpc
         q = k / (13.41*self.k_eq)
         ks = k*self.sound_horizon
@@ -246,20 +241,20 @@ class EHPower(LinearPowerBase):
 
         s_tilde = self.sound_horizon * (1 + (self.beta_node/ks)**3) ** (-1./3.)
         ks_tilde = k*s_tilde
-    
+
         T_b_T0 = f(T_c_ln_nobeta, T_c_C_noalpha)
-        T_b_1 = T_b_T0 / (1 + (ks/5.2)**2 ) 
+        T_b_1 = T_b_T0 / (1 + (ks/5.2)**2 )
         T_b_2 = self.alpha_b / (1 + (self.beta_b/ks)**3 ) * np.exp(-(k/self.k_silk) ** 1.4)
         T_b = np.sinc(ks_tilde/np.pi) * (T_b_1 + T_b_2)
-    
-        T = np.ones(valid.shape) 
+
+        T = np.ones(valid.shape)
         T[valid] = self.f_baryon*T_b + (1-self.f_baryon)*T_c;
         return T
 
 class NoWiggleEHPower(LinearPowerBase):
     """
     Eisenstein & Hu (1998) fitting function without BAO wiggles
-    
+
     From EH 1998, Eqs. 26,28-31.
     """
     def __init__(self, cosmo, redshift):
@@ -274,7 +269,7 @@ class NoWiggleEHPower(LinearPowerBase):
         """
         LinearPowerBase.__init__(self, cosmo, redshift)
         self._set_params()
-        
+
     def _set_params(self):
         """
         Initialize the parameters of the fitting formula
@@ -282,21 +277,21 @@ class NoWiggleEHPower(LinearPowerBase):
         self.Obh2      = self.cosmo.Ob0 * self.cosmo.h ** 2
         self.Omh2      = self.cosmo.Om0 * self.cosmo.h ** 2
         self.f_baryon  = self.cosmo.Ob0 / self.cosmo.Om0
-        self.theta_cmb = self.cosmo.Tcmb0.value / 2.7
-    
+        self.theta_cmb = self.cosmo.Tcmb0 / 2.7
+
         # wavenumber of equality
         self.k_eq = 0.0746 * self.Omh2 * self.theta_cmb ** (-2) # units of 1/Mpc
-    
+
         self.sound_horizon = self.cosmo.h * 44.5 * np.log(9.83/self.Omh2) / np.sqrt(1 + 10 * self.Obh2** 0.75) # in Mpc/h
         self.alpha_gamma = 1 - 0.328 * np.log(431*self.Omh2) * self.f_baryon + 0.38* np.log(22.3*self.Omh2) * self.f_baryon ** 2
-        
-        
+
+
     def transfer(self, k):
         """
         Return the transfer function without BAO wiggles
-        
+
         This is normalized to unity on large scales
-        
+
         Paramters
         ---------
         k : float, array_like
@@ -304,20 +299,20 @@ class NoWiggleEHPower(LinearPowerBase):
         """
         if np.isscalar(k) and k == 0.:
             return 1.0
-            
+
         # only compute k > 0 modes
         k = np.asarray(k)
         valid = k > 0.
-        
+
         k = k[valid] * self.cosmo.h # in 1/Mpc now
         ks = k * self.sound_horizon / self.cosmo.h
         q = k / (13.41*self.k_eq)
-        
+
         gamma_eff = self.Omh2 * (self.alpha_gamma + (1 - self.alpha_gamma) / (1 + (0.43*ks) ** 4))
         q_eff = q * self.Omh2 / gamma_eff
         L0 = np.log(2*np.e + 1.8 * q_eff)
         C0 = 14.2 + 731.0 / (1 + 62.5 * q_eff)
-    
+
         T = np.ones(valid.shape)
         T[valid] = L0 / (L0 + C0 * q_eff**2)
         return T
