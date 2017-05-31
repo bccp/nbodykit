8dcb187bcb29ea74a08f6cd5a6d6d4dde6c049bf
Nick Hand <nhand@berkeley.edu>
2017-05-31 08:07:28 -0400

    import 3pt CF class into algorithms module namespace

--- nbodykit/algorithms/__init__.py
+++ nbodykit/algorithms/__init__.py
@@ -3,3 +3,4 @@ from .fof import FOF
 from .convpower import ConvolvedFFTPower
 from .zhist import RedshiftHistogram
 from .fibercollisions import FiberCollisions
+from .threeptcf import Multipoles3PCF
