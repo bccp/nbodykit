a22d20d6dde681f43bb577c6dcdf183514f6f64d
Yu Feng <rainwoodman@gmail.com>
2017-06-04 23:16:54 -0700

    Allow resampling a memorymesh

--- nbodykit/source/mesh/memory.py
+++ nbodykit/source/mesh/memory.py
@@ -14,9 +14,19 @@ class MemoryMesh(MeshSource):
     def __repr__(self):
         return "MemoryMesh()"
 
-    def __init__(self, field):
+    def __init__(self, field, Nmesh=None):
+        if Nmesh is None:
+            Nmesh = field.Nmesh
+
+        MeshSource.__init__(self, field.pm.comm, Nmesh, field.BoxSize, field.pm.dtype)
+
+        if any(field.Nmesh != self.pm.Nmesh):
+            cnew = ComplexField(self.pm)
+            field = field.resample(out=cnew)
+
+            if self.comm.rank == 0: self.logger.info('resampling done')
+
         self.field = field
-        MeshSource.__init__(self, field.pm.comm, field.Nmesh, field.BoxSize, field.pm.dtype)
 
     def to_complex_field(self):
         if isinstance(self.field, ComplexField):
