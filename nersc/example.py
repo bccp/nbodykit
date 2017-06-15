0133dc55e3ae84ca44447b72c5bbbab28614bb8f
Nick Hand <nhand@berkeley.edu>
2017-06-15 17:14:20 -0400

    update syntax in example

--- nersc/example.py
+++ nersc/example.py
@@ -7,8 +7,8 @@ cosmo = cosmology.Planck15
 
 Plin = cosmology.EHPower(cosmo, redshift=0.55)
 # lognormal particles
-source = Source.LogNormal(Plin=Plin, nbar=3e-7, BoxSize=1380., Nmesh=8, seed=42)
-                
+source = LogNormalCatalog(Plin=Plin, nbar=3e-7, BoxSize=1380., Nmesh=8, seed=42)
+
 # apply RSD
 source['Position'] += source['VelocityOffset'] * [0,0,1]
 
@@ -16,5 +16,5 @@ source['Position'] += source['VelocityOffset'] * [0,0,1]
 result = FFTPower(source, mode='2d', poles=[0,2,4], los=[0,0,1])
 
 # and save
-output = "./test_zeldovich.json"
+output = "./nbkit_example_power.json"
 result.save(output)
