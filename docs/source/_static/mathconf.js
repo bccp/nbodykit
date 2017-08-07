224d93235676835ebc3ece738ca4d0642ab61279
Nick Hand <nhand@berkeley.edu>
2017-08-07 18:27:36 -0400

    add custom latex commands for use with mathjax in sphinx docs

---
+++ docs/source/_static/mathconf.js
@@ -0,0 +1,22 @@
+MathJax.Hub.Config({
+  TeX: {
+    Macros: {
+      d: '\\mathrm{d}',
+      L: '\\mathcal{L}',
+      vk: '\\mathbf{k}',
+      vkhat: '\\mathbf{\\hat{k}}',
+      vrhat: '\\mathbf{\\hat{r}}',
+      vnhat: '\\mathbf{\\hat{n}}',
+      vq: '\\mathbf{q}',
+      vx: '\\mathbf{x}',
+      vs: '\\mathbf{s}',
+      vr: '\\mathbf{r}',
+      vrone: '\\mathbf{r}_1',
+      vrtwo: '\\mathbf{r}_2',
+      obs: '\\textrm{obs}',
+      Ylm: 'Y_{\\ell m}',
+      l: '\\ell',
+      wfkp: 'w_\\mathrm{FKP}',
+    }
+  }
+});
