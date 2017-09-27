be25f20a27048580a57d5073ca97bad28f363f05
Yu Feng <rainwoodman@gmail.com>
2017-09-27 14:43:14 -0700

    Add stricter tests for FOF.
    I noticed the output of FastPM fof catalog shows some variation
    going from 1 to 4 ranks. The difference is concentrated near the
    edges or domains, and it can affect even relatively large halos
    to some extent. (though I was looking at a 5 step simulation.)
    
    I still do not know what is causing the difference.
    
    I checked that the position of particles are 1e-6 identical.
    
    These FOF tests asserts the behavior of the parallel merging is
    correct with simple cases that goes cross the domain boundary.

--- nbodykit/algorithms/tests/test_fof.py
+++ nbodykit/algorithms/tests/test_fof.py
@@ -2,6 +2,8 @@ from runtests.mpi import MPITest
 from nbodykit.lab import *
 from nbodykit import setup_logging
 
+from numpy.testing import assert_allclose
+
 # debug logging
 setup_logging("debug")
 
@@ -22,3 +24,39 @@ def test_fof(comm):
     # save the halos
     peaks = fof.find_features(peakcolumn='Density')
     peaks.save("FOF-%d" % comm.size, ['CMPosition', 'CMVelocity', 'Length', 'PeakPosition', 'PeakVelocity'])
+
+@MPITest([1, 4])
+def test_fof_parallel_no_merge(comm):
+    CurrentMPIComm.set(comm)
+    from pmesh.pm import ParticleMesh
+    pm = ParticleMesh(BoxSize=[8, 8, 8], Nmesh=[8, 8, 8], comm=comm)
+    Q = pm.generate_uniform_particle_grid()
+    cat = ArrayCatalog({'Position' : Q}, BoxSize=pm.BoxSize, Nmesh=pm.Nmesh)
+
+    fof = FOF(cat, linking_length=0.9, nmin=0)
+    
+    labels = numpy.concatenate(comm.allgather((fof.labels)), axis=0)
+    # one particle per group
+    assert max(labels) == cat.csize - 1
+
+@MPITest([1, 4])
+def test_fof_parallel_merge(comm):
+    CurrentMPIComm.set(comm)
+    from pmesh.pm import ParticleMesh
+    pm = ParticleMesh(BoxSize=[32, 32, 32], Nmesh=[32, 32, 32], comm=comm)
+    Q = pm.generate_uniform_particle_grid(shift=0)
+    Q1 = Q.copy()
+    Q1[:] += 0.01
+    Q2 = Q.copy()
+    Q2[:] -= 0.01
+    Q3 = Q.copy()
+    Q3[:] += 0.02
+    cat = ArrayCatalog({'Position' : 
+            numpy.concatenate([Q, Q1, Q2, Q3], axis=0)}, BoxSize=pm.BoxSize, Nmesh=pm.Nmesh)
+
+    fof = FOF(cat, linking_length=0.011 * 3 ** 0.5, nmin=0, absolute=True)
+
+    labels = numpy.concatenate(comm.allgather((fof.labels)), axis=0)
+    assert max(labels) == pm.Nmesh.prod() - 1
+    assert all(numpy.bincount(labels) == 4)
+
