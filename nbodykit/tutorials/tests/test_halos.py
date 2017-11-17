904a987692c383d59c48f19ba441d73e2caf7342
Nick Hand <nhand@berkeley.edu>
2017-11-17 14:51:42 -0500

    add tests of downloading

---
+++ nbodykit/tutorials/tests/test_halos.py
@@ -0,0 +1,34 @@
+from runtests.mpi import MPITest
+from nbodykit.tutorials import DemoHaloCatalog
+from nbodykit import setup_logging, CurrentMPIComm
+import pytest
+
+setup_logging()
+
+@MPITest([4])
+def test_download(comm):
+
+    from halotools.sim_manager import UserSuppliedHaloCatalog
+    CurrentMPIComm.set(comm)
+
+    # download and load the cached catalog
+    cat = DemoHaloCatalog('bolshoi', 'rockstar', 0.5)
+    assert all(col in cat for col in ['Position', 'Velocity'])
+
+    # convert to halotools catalog
+    halotools_cat = cat.to_halotools()
+    assert isinstance(halotools_cat, UserSuppliedHaloCatalog)
+
+    # bad simulation name
+    with pytest.raises(Exception):
+        cat = DemoHaloCatalog('BAD', 'rockstar', 0.5)
+
+
+@MPITest([4])
+def test_download_failure(comm):
+    CurrentMPIComm.set(comm)
+
+    # initialize with bad redshift
+    BAD_REDSHIFT = 100.0
+    with pytest.raises(Exception):
+        cat = DemoHaloCatalog('bolshoi', 'rockstar', BAD_REDSHIFT)
---
+++ nbodykit/tutorials/tests/test_wget.py
@@ -0,0 +1,57 @@
+from runtests.mpi import MPITest
+from nbodykit.tutorials import download_example_data
+from nbodykit import setup_logging, CurrentMPIComm
+import pytest
+import shutil
+import os
+import tempfile
+
+setup_logging()
+
+@MPITest([1])
+def test_download_directory(comm):
+
+    # download Gadget1P snapshots directory
+    filename = 'Gadget1P'
+    download_example_data(filename)
+
+    # check that it worked
+    assert filename in os.listdir('.')
+    assert len(os.listdir(filename)) > 0
+
+    # remove the downloaded file
+    shutil.rmtree(filename)
+
+@MPITest([1])
+def test_download_file(comm):
+
+    # download Gadget1P snapshots directory
+    filename = 'tpm_1.0000.bin.00'
+    download_example_data(filename)
+
+    # check that it worked
+    assert filename in os.listdir('.')
+
+    # remove filename
+    os.remove(filename)
+
+@MPITest([1])
+def test_download_failure(comm):
+
+    filename = 'MISSING'
+    with pytest.raises(Exception):
+        download_example_data(filename)
+
+@MPITest([1])
+def test_download_to_location(comm):
+
+    # download Gadget1P snapshots directory
+    filename = 'Gadget1P'
+    loc = tempfile.mkdtemp()
+    download_example_data(filename, download_dirname=loc)
+
+    # check that it worked
+    assert filename in os.listdir(loc)
+
+    # delete temp directory
+    shutil.rmtree(loc)
