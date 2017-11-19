bc25291d2111877d94b7530a3fa38d8c4089885a
Nick Hand <nhand@berkeley.edu>
2017-11-19 13:32:44 -0500

    add pair counting test

---
+++ benchmarks/test_paircount.py
@@ -0,0 +1,24 @@
+from nbodykit.lab import *
+from nbodykit import setup_logging
+import pytest
+
+setup_logging()
+
+def test_strong_scaling(benchmark, sample):
+
+    # generate fake data
+    with benchmark("Data"):
+        data = sample.data(seed=42)
+
+    # run
+    with benchmark("Algorithm"):
+
+        # r binning
+        nbins = 10
+        edges = numpy.linspace(10., 150.0, nbins+1)
+
+        # run the algorithm
+        r = SimulationBoxPairCount('1d', data, redges, periodic=True)
+
+    # save meta-data
+    benchmark.attrs.update(N=sample.N, sample=sample.name)
