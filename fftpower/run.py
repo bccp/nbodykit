404b33011eb6d55da7f91e2c10e038f751e0ea59
Nick Hand <nhand@berkeley.edu>
2017-11-18 19:57:45 -0500

    reorganize tests

--- fftpower/run.py
+++ fftpower/run.py
@@ -1,37 +1,22 @@
 #! /usr/bin/env python
 import sys, os
 sys.path.insert(0, os.path.abspath('..'))
-from runner import BenchmarkRunner, parametrize
+from runner import BenchmarkRunner
 
-# extract info from current path
-this_dir = os.path.normpath(os.path.abspath(os.path.split(__file__)[0]))
-results_dir = os.path.join(this_dir, 'results')
-algorithm = this_dir.split(os.path.sep)[-1]
+# current directory
+this_dir = os.path.abspath(os.path.dirname(__file__))
 
 # CONFIGURATION
-CORES = [64, 128, 256, 512]
-SAMPLES = ['boss', 'desi']
-TESTNAMES = ['test_strong_scaling']
-
-@parametrize({'sample': SAMPLES, 'testname':TESTNAMES, 'ncores':CORES})
-def add_commands(sample, testname, ncores):
-
-    # the name of the benchmark test to run
-    bench_name = os.path.join('benchmarks', 'test_' + algorithm + '.py')
-    bench_name += "::" + testname
-
-    # the output directory
-    bench_dir = os.path.join(results_dir, sample, str(ncores))
-
-    # make the command
-    args = (bench_name, sample, bench_dir, ncores)
-    cmd = "python ../benchmark.py {} --sample {} --bench-dir {} -n {}".format(*args)
-
-    # and register
-    tag = {'sample':sample, 'testname':testname, 'ncores':ncores}
-    BenchmarkRunner.register(cmd, tag=tag)
+ncores = [64, 128, 256, 512]
+test_functions = ['test_strong_scaling']
 
 
 if __name__ == '__main__':
-    add_commands()
-    BenchmarkRunner.execute()
+
+    result_dir = os.path.join(this_dir, 'results') # output directory
+    test_path = 'benchmarks/test_fftpower.py' # the test file we want to run
+
+    # initialize and run
+    runner = BenchmarkRunner(test_path, result_dir)
+    runner.add_commands(test_functions, ncores)
+    runner.execute()
