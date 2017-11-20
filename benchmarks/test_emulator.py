08d1ddfbea2a7114a09b7950d3b77839f93700ec
Nick Hand <nhand@berkeley.edu>
2017-11-20 14:46:03 -0500

    update emuator benchmark

--- benchmarks/test_emulator.py
+++ benchmarks/test_emulator.py
@@ -21,10 +21,20 @@ def test_strong_scaling(benchmark):
         linear = LinearMesh(power, BoxSize=512, Nmesh=512)
         sim = FastPMCatalogSource(linear, Nsteps=10)
 
+    with benchmark("FFTPower-Matter"):
+        # compute and save matter P(k)
+        r = FFTPower(sim, mode="1d", Nmesh=512)
+        r.save("matter-power.json")
+
     # run FOF to identify halo groups
     with benchmark("FOF"):
         fof = FOF(sim, 0.2, nmin=20)
         halos = fof.to_halos(1e12, cosmo, 0.)
+
+    with benchmark("FFTPower-Halo"):
+        # compute and save halo P(k)
+        r = FFTPower(halos, mode="1d", Nmesh=512)
+        r.save("halos-power.json")
 
     # populate halos with galaxies
     with benchmark("HOD"):
@@ -32,6 +42,6 @@ def test_strong_scaling(benchmark):
 
     # compute and save galaxy power spectrum
     # result consistent on all ranks
-    with benchmark("FFTPower"):
-        r = FFTPower(hod, mode='1d', Nmesh=128)
+    with benchmark("FFTPower-Galaxy"):
+        r = FFTPower(hod, mode='1d', Nmesh=512)
         r.save('galaxy-power.json')
