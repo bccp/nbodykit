352a90d23233db4c2a4df0e64d0b0380741603a7
Yu Feng <rainwoodman@gmail.com>
2018-04-30 15:32:34 -0700

    Add rsd reconstruction.

--- nbodykit/algorithms/fftrecon.py
+++ nbodykit/algorithms/fftrecon.py
@@ -32,10 +32,18 @@ class FFTRecon(MeshSource):
     Nmesh : int
         The size of the FFT Mesh. Rule of thumb is that the size of a mesh cell
         shall be 2 ~ 4 times smaller than the smoothing length, `R`.
+    revert_rsd_random : boolean
+        Revert the rsd for randoms as well as data. There are two conventions.
+        either reverting rsd displacement in data displacement only(False) or
+        in both data and randoms (True). Default is False.
     R : float
         The radius of smoothing. 10 to 20 Mpc/h is usually cool.
     bias : float
         The bias of the data catalog.
+    f: float
+        The growth rate; if non-zero, correct for RSD
+    los : list 
+        The direction of the line of sight for RSD. Usually (default) [0, 0, 1].
     position: string
         column to use for picking up the Position of the objects.
     BoxSize : float or array_like
@@ -49,8 +57,11 @@ class FFTRecon(MeshSource):
             ran,
             Nmesh,
             bias=1.0,
+            f = 0.0,
+            los = [0, 0, 1],
             R=20,
             position='Position',
+            revert_rsd_random=False,
             BoxSize=None,
             comm=None):
 
@@ -67,6 +78,12 @@ class FFTRecon(MeshSource):
         if BoxSize is None:
             BoxSize = data.attrs['BoxSize']
 
+        los = numpy.array(los, dtype='f8', copy=True)
+        los /= (los ** 2).sum()
+
+        assert len(los) == 3
+        assert (~numpy.isnan(los)).all()
+
         pm = ParticleMesh(BoxSize=BoxSize, Nmesh=_Nmesh, comm=comm)
         self.pm = pm
 
@@ -78,7 +95,10 @@ class FFTRecon(MeshSource):
         MeshSource.__init__(self, comm, pm.Nmesh.copy(), pm.BoxSize.copy(), pm.dtype)
 
         self.attrs['bias'] = bias
+        self.attrs['f'] = f
+        self.attrs['los'] = los
         self.attrs['R'] = R
+        self.attrs['revert_rsd_random'] = bool(revert_rsd_random)
 
         self.data = data
         self.ran = ran
@@ -88,7 +108,6 @@ class FFTRecon(MeshSource):
         return self.run()
 
     def run(self):
-
         dpos = self.data[self.position].compute()
         rpos = self.ran[self.position].compute()
 
@@ -96,7 +115,7 @@ class FFTRecon(MeshSource):
         return self._paint(dpos - s_d, rpos - s_r)
 
     def _paint(self, dpos, rpos):
-
+        """ Convert the displacements of data and random to a single reconstruction mesh object. """
         nbar_d = (self.data.csize / self.pm.Nmesh.prod())
         nbar_r = (self.ran.csize / self.pm.Nmesh.prod())
 
@@ -111,7 +130,8 @@ class FFTRecon(MeshSource):
         return delta_d - delta_r
 
     def _compute_s(self, dpos, rpos):
-        
+        """ Computing the reconstruction displacement of data and random """
+
         nbar_d = (self.data.csize / self.pm.Nmesh.prod())
         nbar_r = (self.ran.csize / self.pm.Nmesh.prod())
 
@@ -119,7 +139,17 @@ class FFTRecon(MeshSource):
             def kernel(k, v):
                 k2 = sum(ki**2 for ki in k)
                 k2[k2 == 0] = 1.0
-                return 1j * k[d] / k2 * v * numpy.exp(-0.5 * k2 * self.attrs['R']**2) / self.attrs['bias']
+
+                # reverting rsd.
+                mu = sum(k[i] * self.attrs['los'][i] for i in range(len(k))) / k2 ** 0.5
+
+                v = v * numpy.exp(-0.5 * k2 * self.attrs['R']**2)
+
+                frac = self.attrs['bias'] * ( 1 + self.attrs['f'] / self.attrs['bias'] * mu ** 2)
+
+                v = v / frac
+
+                return 1j * k[d] / k2 * v
             return kernel
 
         layout = self.pm.decompose(dpos)
@@ -138,5 +168,12 @@ class FFTRecon(MeshSource):
             s_d[..., d] = tmp.readout(dpos, layout=layout)
             s_r[..., d] = tmp.readout(rpos, layout=rlayout)
 
+        # convention 1: shifting data only
+        s_d *= (1 + self.attrs['los'] * self.attrs['f'])
+
+        # convention 2: shifting data only
+        if self.attrs['revert_rsd_random']:
+            s_r *= (1 + self.attrs['los'] * self.attrs['f'])
+        
         return s_d, s_r
 
