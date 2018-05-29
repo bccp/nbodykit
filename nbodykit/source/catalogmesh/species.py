f2eb8e680daf276c6f009581eafe082207ccf24c
Yu Feng <rainwoodman@gmail.com>
2018-05-29 14:41:50 -0700

    value not values.

--- nbodykit/source/catalogmesh/species.py
+++ nbodykit/source/catalogmesh/species.py
@@ -107,7 +107,7 @@ class MultipleSpeciesCatalogMesh(CatalogMesh):
         attrs = {'num_per_cell':0., 'N':0}
 
         # initialize an empty real field
-        real = self.pm.create(mode='real', values=0)
+        real = self.pm.create(mode='real', value=0)
 
         # loop over each species
         for name in self.base.species:
