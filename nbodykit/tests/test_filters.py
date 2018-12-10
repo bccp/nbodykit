2f17a8bb2f71df30bca0691df012430aed59284e
Yu Feng <rainwoodman@gmail.com>
2018-12-09 20:06:15 -0800

    update tests.

--- nbodykit/tests/test_filters.py
+++ nbodykit/tests/test_filters.py
@@ -14,10 +14,9 @@ def test_tophat(comm):
     from nbodykit.base.mesh import MeshFilter
 
     cosmo = cosmology.Planck15
-    CurrentMPIComm.set(comm)
 
     Plin = cosmology.LinearPower(cosmo, redshift=0.55, transfer='EisensteinHu')
-    source = LinearMesh(Plin, Nmesh=64, BoxSize=512, seed=42)
+    source = LinearMesh(Plin, Nmesh=64, BoxSize=512, seed=42, comm=comm)
     source2 = source.apply(TopHat(8))
 
     r2 = source2.paint()
@@ -28,10 +27,9 @@ def test_tophat(comm):
     from nbodykit.base.mesh import MeshFilter
 
     cosmo = cosmology.Planck15
-    CurrentMPIComm.set(comm)
 
     Plin = cosmology.LinearPower(cosmo, redshift=0.55, transfer='EisensteinHu')
-    source = LinearMesh(Plin, Nmesh=64, BoxSize=512, seed=42)
+    source = LinearMesh(Plin, Nmesh=64, BoxSize=512, seed=42, comm=comm)
     source2 = source.apply(Gaussian(8))
     r2 = source2.paint()
     # FIXME: add numerical assertions
