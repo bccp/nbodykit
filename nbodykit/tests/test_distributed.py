8986fdfe2b9e2c3899d757a22eb533b3a9951be7
Yu Feng <rainwoodman@gmail.com>
2021-05-08 03:50:21 -0700

    Workaround https://github.com/dask/distributed/issues/4565
    As we only intend to use distributed with a single MPI rank
    configuration, the test coverage is sufficient after this workaround.

--- nbodykit/tests/test_distributed.py
+++ nbodykit/tests/test_distributed.py
@@ -5,9 +5,13 @@ from nbodykit.lab import UniformCatalog
 import pytest
 
 def setup():
-    from distributed import LocalCluster, Client
-    cluster = LocalCluster(n_workers=1, threads_per_worker=1, processes=False)
-    use_distributed(Client(cluster))
+    # only initializes the local cluster this on the root rank.
+    # all unit tests in this file must be protected by MPITest([1]).
+    from mpi4py import MPI
+    if MPI.COMM_WORLD.rank == 0:
+        from distributed import LocalCluster, Client
+        cluster = LocalCluster(n_workers=1, threads_per_worker=1, processes=False)
+        use_distributed(Client(cluster))
 
 def teardown():
     from mpi4py import MPI
@@ -19,13 +23,10 @@ def test_pickle(comm):
     cat = UniformCatalog(1e-3, 512, comm=comm)
     ss = pickle.dumps(cat)
     cat2 = pickle.loads(ss)
-    
+
     assert_array_equal(cat['Position'], cat2['Position'])
 
-import distributed
 
-#@pytest.mark.xfail(distributed.__version__ in ['2021.03.0', '2021.04.0'],
-#    reason="https://github.com/dask/distributed/issues/4565")
 @MPITest([1])
 def test_save(comm):
     cat = UniformCatalog(1e-3, 512, comm=comm)
